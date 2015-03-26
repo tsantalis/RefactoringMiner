75def21aa9e179fd0cb4db959db2518059b1cfc1
Danilo F. Silva <danilofes@gmail.com>
2015-03-25 23:16:46 -0300

    Script para monitoramento.

--- scripts/monitor-processes.sql
+++ scripts/monitor-processes.sql
@@ -1,6 +1,6 @@
 select 
   p.running_pid as "ID do Processo", 
-  p.name as "Nome", p.cloneUrl as "Clone URL", 
+  p.name as "Projeto Atual", p.cloneUrl as "URL do Repositorio", 
   format(p.size/1000, 1) as "Tamanho (MB)", 
   format(p.commits_count, 0) as "Commits",
   format(100 * (select count(*) from revisiongit rev where rev.project = p.id)/p.commits_count, 1) as "Progresso (%)"

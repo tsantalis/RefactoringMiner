688856c5ff7ff8f1e3e77d997342c7ab6296467f
Danilo F. Silva <danilofes@gmail.com>
2015-03-26 16:00:26 -0300

    Configuring log file path dinamically.

--- src/br/ufmg/dcc/labsoft/refactoringanalyzer/operations/AnalyzeProjects.java
+++ src/br/ufmg/dcc/labsoft/refactoringanalyzer/operations/AnalyzeProjects.java
@@ -3,6 +3,8 @@ package br.ufmg.dcc.labsoft.refactoringanalyzer.operations;
 import java.io.File;
 import java.lang.management.ManagementFactory;
 
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.PatternLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +13,7 @@ import br.ufmg.dcc.labsoft.refactoringanalyzer.dao.ProjectGit;
 
 public class AnalyzeProjects {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		final Logger logger = LoggerFactory.getLogger(AnalyzeProjects.class);
 		final Database db = new Database();
 		final String pid = ManagementFactory.getRuntimeMXBean().getName();
@@ -24,12 +26,32 @@ public class AnalyzeProjects {
             }
         });
 		
-		GitProjectAnalyzer analyzer = new GitProjectAnalyzer(new File("tmp"), db);
-		ProjectGit project = null;
-		while ((project = db.findNonAnalyzedProjectAndLock(pid)) != null) {
-			analyzer.analyzedProject(project);
+		try {
+			File workingDir = new File("tmp");
+			if (args.length > 0) {
+				workingDir = new File(args[0]);
+			}
+			if (!workingDir.exists()) {
+				workingDir.mkdir();
+			}
+			// setting up a FileAppender dynamically...
+			String logFilePath = workingDir.getPath() + "/rd.log";
+			logger.info("Log file: " + logFilePath);
+			
+			PatternLayout layout = new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %-5p %m%n");
+			FileAppender appender = new FileAppender(layout, logFilePath, true);
+			org.apache.log4j.Logger.getRootLogger().addAppender(appender);
+			
+			GitProjectAnalyzer analyzer = new GitProjectAnalyzer(workingDir, db);
+			ProjectGit project = null;
+			while ((project = db.findNonAnalyzedProjectAndLock(pid)) != null) {
+				analyzer.analyzedProject(project);
+			}
+			logger.info("No more projects to analyze");
+			
+		} catch (Exception e) {
+			logger.error("Error", e);
 		}
-		logger.info("No more projects to analyze");
 	}
 
 }

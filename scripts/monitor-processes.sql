ea6fbdcf7de7004a6b2cc0b7b217a37342d1cb26
Danilo F. Silva <danilofes@gmail.com>
2015-03-25 23:15:42 -0300

    Script para monitorar processos.

---
+++ scripts/monitor-processes.sql
@@ -0,0 +1,8 @@
+select 
+  p.running_pid as "ID do Processo", 
+  p.name as "Nome", p.cloneUrl as "Clone URL", 
+  format(p.size/1000, 1) as "Tamanho (MB)", 
+  format(p.commits_count, 0) as "Commits",
+  format(100 * (select count(*) from revisiongit rev where rev.project = p.id)/p.commits_count, 1) as "Progresso (%)"
+from projectgit p
+where p.running_pid <> 'wait';

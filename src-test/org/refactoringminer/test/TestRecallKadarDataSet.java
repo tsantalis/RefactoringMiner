b4e4302cc406b6a2f03ca59343740e1baae30a2b
tsantalis <tsantalis@gmail.com>
2016-08-30 15:04:29 -0400

    Fixed test to have the correct Inline Method refactoring description

--- src-test/org/refactoringminer/test/TestRecallKadarDataSet.java
+++ src-test/org/refactoringminer/test/TestRecallKadarDataSet.java
@@ -21,7 +21,7 @@ public class TestRecallKadarDataSet {
 						"Extract Method addTestsFromTestCase(theClass final Clss<?>) : void extracted from public TestSuite(final Class<? extends TestCase> theClass) in class in class junit.src.main.java.junit.framework.TestSuite",
 						"Rename Method	public testsThatAreBothIncludedAndExcludedAreIncluded() : void renamed to public testsThatAreBothIncludedAndExcludedAreExcluded() : void in class org.junit.tests.experimental.categories.CategoryTest",
 						"Rename Method	public saffSqueezeExample() : void renamed to public filterSingleMethodFromOldTestClass() : void in class org.junit.tests.experimental.max.MaxStarterTest",
-						"Inline Method	private ruleFields() : List<FrameworkField> inlined to private  validateFields(errors List<Throwable>) : void  in class junit.src.main.java.org.junit.runners.BlockJUnit4ClassRunner");
+						"Inline Method	private ruleFields() : List<FrameworkField> inlined to private validateFields(errors List<Throwable>) : void in class org.junit.runners.BlockJUnit4ClassRunner");
 	
 		test.assertExpectations();
 	}

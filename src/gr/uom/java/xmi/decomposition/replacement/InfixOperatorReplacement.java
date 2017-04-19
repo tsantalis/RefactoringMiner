e59b753a9d4b8cf3df7ec42670286b030af672e6
tsantalis <tsantalis@gmail.com>
2017-04-19 08:49:11 -0400

    Added new class InfixOperatorReplacement

---
+++ src/gr/uom/java/xmi/decomposition/replacement/InfixOperatorReplacement.java
@@ -0,0 +1,9 @@
+package gr.uom.java.xmi.decomposition.replacement;
+
+public class InfixOperatorReplacement extends Replacement {
+
+	public InfixOperatorReplacement(String before, String after) {
+		super(before, after);
+	}
+
+}

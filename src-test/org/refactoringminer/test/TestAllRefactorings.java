4a47474b394aa57b04e9e6c9eabc73573e30870d
tsantalis <tsantalis@gmail.com>
2017-06-08 10:15:30 -0400

    Some fixes in TestAllRefactorings

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -1,9 +1,6 @@
 package org.refactoringminer.test;
 
-import static org.junit.Assert.*;
-
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
-import org.refactoringminer.rm2.analysis.GitHistoryRefactoringMiner2;
 import org.refactoringminer.test.RefactoringPopulator.Refactorings;
 import org.refactoringminer.test.RefactoringPopulator.Systems;
 import org.junit.Test;
@@ -17,12 +14,9 @@ public class TestAllRefactorings {
 
 		TestBuilder test = new TestBuilder(new GitHistoryRefactoringMinerImpl(), "tmp1", refactoring);
 
-		RefactoringPopulator.feedRefactoringsInstances(refactoring, refactoring, test);
+		RefactoringPopulator.feedRefactoringsInstances(refactoring, Systems.FSE.getValue(), test);
 
 		RefactoringPopulator.printRefDiffResults(refactoring);
 		test.assertExpectations();
-		
 	}
-	
-	public void generateTbaleReult(){}
 }

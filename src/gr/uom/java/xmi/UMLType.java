52bf66c0ec3dc5090fcc12ea4c5aa1e9caebd4c8
tsantalis <tsantalis@gmail.com>
2018-07-05 00:51:20 -0400

    Improved the comparison of generic types.

--- src/gr/uom/java/xmi/UMLType.java
+++ src/gr/uom/java/xmi/UMLType.java
@@ -1,17 +1,22 @@
 package gr.uom.java.xmi;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class UMLType implements Serializable {
     private String classType;
     private String genericType;
     private int arrayDimension;
+    private List<String> genericTypeDecomposition;
     private volatile int hashCode = 0;
-    private static Pattern pattern = Pattern.compile("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+    private static final Pattern CAMEL_CASE_SPLIT_PATTERN = Pattern.compile("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
+    private static final Pattern GENERIC_TYPE_SPLIT_PATTERN = Pattern.compile("<|>|,|\\s");
 
     public UMLType(String type) {
         this.classType = type;
+        this.genericTypeDecomposition = new ArrayList<String>();
     }
 
     public String getClassType() {
@@ -22,19 +27,11 @@ public class UMLType implements Serializable {
         return genericType;
     }
 
-    public void setGeneric(String g) {
-        this.genericType = g;
-    }
-    
-    public void setArrayDimension(int dimension) {
-    	this.arrayDimension = dimension;
-    }
-    
     public int getArrayDimension() {
     	return this.arrayDimension;
     }
 
-    public boolean equals(Object o) {
+	public boolean equals(Object o) {
         if(this == o) {
             return true;
         }
@@ -63,8 +60,8 @@ public class UMLType implements Serializable {
     }
 
 	private static boolean lastCamelCaseTokenMatch(String classType1, String classType2) {
-		String[] tokens1 = pattern.split(classType1);
-		String[] tokens2 = pattern.split(classType2);
+		String[] tokens1 = CAMEL_CASE_SPLIT_PATTERN.split(classType1);
+		String[] tokens2 = CAMEL_CASE_SPLIT_PATTERN.split(classType2);
 		if(tokens1.length > 0 && tokens2.length > 0) {
 			return tokens1[tokens1.length-1].equals(tokens2[tokens2.length-1]);
 		}
@@ -76,7 +73,26 @@ public class UMLType implements Serializable {
 				(this.genericType.startsWith("<? ") && typeObject.genericType.equals("<?>"))) {
 			return true;
 		}
-		return this.genericType.equals(typeObject.genericType);
+		if(this.genericTypeDecomposition.size() != typeObject.genericTypeDecomposition.size()) {
+			return false;
+		}
+		for(int i=0; i<this.genericTypeDecomposition.size(); i++) {
+			String thisComponent = this.genericTypeDecomposition.get(i);
+			String otherComponent = typeObject.genericTypeDecomposition.get(i);
+			int thisLength = thisComponent.length();
+			int otherLength = otherComponent.length();
+			if(thisLength > 1 && otherLength > 1 && !thisComponent.equals(otherComponent)) {
+				return false;
+			}
+			if(thisLength == 1 && otherLength > 1) {
+				return false;
+			}
+			if(thisLength > 1 && otherLength == 1) {
+				return false;
+			}
+			//type parameter names, such as E, K, N, T, V, S, U, having a length of 1 are always equal
+		}
+		return true;
 	}
 
     public int hashCode() {
@@ -101,7 +117,7 @@ public class UMLType implements Serializable {
         return sb.toString();
     }
 
-    private String simpleNameOf(String name) {
+    private static String simpleNameOf(String name) {
     	int numberOfDots = 0;
     	for (int i = 0; i < name.length(); i++) {
     		if (name.charAt(i) == '.') {
@@ -118,6 +134,7 @@ public class UMLType implements Serializable {
 	public static UMLType extractTypeObject(String qualifiedName) {
 		int arrayDimension = 0;
 		String generic = null;
+		String[] genericTypeDecomposition = {};
 		if(qualifiedName.endsWith("[]")) {
 			while(qualifiedName.endsWith("[]")) {
 				qualifiedName = qualifiedName.substring(0, qualifiedName.lastIndexOf("[]"));
@@ -127,10 +144,14 @@ public class UMLType implements Serializable {
 		if(qualifiedName.contains("<") && qualifiedName.contains(">")) {
 			generic = qualifiedName.substring(qualifiedName.indexOf("<"), qualifiedName.lastIndexOf(">")+1);
 			qualifiedName = qualifiedName.substring(0, qualifiedName.indexOf("<"));
+			genericTypeDecomposition = GENERIC_TYPE_SPLIT_PATTERN.split(generic);
 		}
 		UMLType typeObject = new UMLType(qualifiedName);
-		typeObject.setGeneric(generic);
-		typeObject.setArrayDimension(arrayDimension);
+		typeObject.genericType = generic;
+		typeObject.arrayDimension = arrayDimension;
+		for(String type : genericTypeDecomposition) {
+			typeObject.genericTypeDecomposition.add(simpleNameOf(type));
+		}
 		return typeObject;
 	}
 }

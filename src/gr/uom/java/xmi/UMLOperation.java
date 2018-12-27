a4b09bcd552e4cce7b1838fa54117a378a8468b5
tsantalis <tsantalis@gmail.com>
2018-12-27 16:47:14 -0500

    Added support for the detection of Extract Class and Extract Subclass refactorings.

--- src/gr/uom/java/xmi/UMLOperation.java
+++ src/gr/uom/java/xmi/UMLOperation.java
@@ -208,14 +208,14 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 	}
 
 	public boolean equalSignatureIgnoringChangedTypes(UMLOperation operation) {
-		if(!(this.isConstructor && operation.isConstructor || this.name.equals(operation.name)))
+		if(!(this.isConstructor && operation.isConstructor || equivalentName(operation)))
 			return false;
 		if(this.isAbstract != operation.isAbstract)
 			return false;
-		if(this.isStatic != operation.isStatic)
+		/*if(this.isStatic != operation.isStatic)
 			return false;
 		if(this.isFinal != operation.isFinal)
-			return false;
+			return false;*/
 		if(this.parameters.size() != operation.parameters.size())
 			return false;
 		int i=0;
@@ -226,6 +226,20 @@ public class UMLOperation implements Comparable<UMLOperation>, Serializable, Loc
 			i++;
 		}
 		return true;
+	}
+
+	private boolean equivalentName(UMLOperation operation) {
+		return this.name.equals(operation.name) || equivalentNames(this, operation) || equivalentNames(operation, this);
+	}
+
+	private static boolean equivalentNames(UMLOperation operation1, UMLOperation operation2) {
+		boolean equalReturn = operation1.equalReturnParameter(operation2) && operation1.getParametersWithoutReturnType().size() > 0 && operation2.getParametersWithoutReturnType().size() > 0;
+		if(operation1.name.startsWith(operation2.name) && !operation2.name.equals("get") && !operation2.name.equals("set")) {
+			String suffix1 = operation1.name.substring(operation2.name.length(), operation1.name.length());
+			String className2 = operation2.className.contains(".") ? operation2.className.substring(operation2.className.lastIndexOf(".")+1, operation2.className.length()) : operation2.className;
+			return operation2.name.length() > operation1.name.length() - operation2.name.length() || equalReturn || className2.contains(suffix1);
+		}
+		return false;
 	}
 
 	public List<UMLParameter> getParametersWithoutReturnType() {
---
+++ src/gr/uom/java/xmi/diff/ExtractClassRefactoring.java
@@ -0,0 +1,66 @@
+package gr.uom.java.xmi.diff;
+
+import java.util.Set;
+
+import org.refactoringminer.api.Refactoring;
+import org.refactoringminer.api.RefactoringType;
+
+import gr.uom.java.xmi.UMLAttribute;
+import gr.uom.java.xmi.UMLClass;
+import gr.uom.java.xmi.UMLOperation;
+
+public class ExtractClassRefactoring implements Refactoring {
+	private UMLClass extractedClass;
+	private UMLClass originalClass;
+	private Set<UMLOperation> extractedOperations;
+	private Set<UMLAttribute> extractedAttributes;
+	private UMLAttribute attributeOfExtractedClassTypeInOriginalClass;
+
+	public ExtractClassRefactoring(UMLClass extractedClass, UMLClass originalClass,
+			Set<UMLOperation> extractedOperations, Set<UMLAttribute> extractedAttributes, UMLAttribute attributeOfExtractedClassType) {
+		this.extractedClass = extractedClass;
+		this.originalClass = originalClass;
+		this.extractedOperations = extractedOperations;
+		this.extractedAttributes = extractedAttributes;
+		this.attributeOfExtractedClassTypeInOriginalClass = attributeOfExtractedClassType;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(getName()).append("\t");
+		sb.append(extractedClass);
+		sb.append(" from class ");
+		sb.append(originalClass);
+		return sb.toString();
+	}
+
+	public RefactoringType getRefactoringType() {
+		if(extractedClass.isSubTypeOf(originalClass))
+			return RefactoringType.EXTRACT_SUBCLASS;
+		return RefactoringType.EXTRACT_CLASS;
+	}
+
+	public String getName() {
+		return this.getRefactoringType().getDisplayName();
+	}
+
+	public UMLClass getExtractedClass() {
+		return extractedClass;
+	}
+
+	public UMLClass getOriginalClass() {
+		return originalClass;
+	}
+
+	public Set<UMLOperation> getExtractedOperations() {
+		return extractedOperations;
+	}
+
+	public Set<UMLAttribute> getExtractedAttributes() {
+		return extractedAttributes;
+	}
+
+	public UMLAttribute getAttributeOfExtractedClassTypeInOriginalClass() {
+		return attributeOfExtractedClassTypeInOriginalClass;
+	}
+}

f78a2847ddc6d1d7b6fe03afae29158ca6b02fe4
tsantalis <tsantalis@gmail.com>
2019-04-13 23:20:04 -0400

    Added infrastructure for the detection of merged variables.

---
+++ src/gr/uom/java/xmi/decomposition/replacement/MergeVariableReplacement.java
@@ -0,0 +1,16 @@
+package gr.uom.java.xmi.decomposition.replacement;
+
+import java.util.Set;
+
+public class MergeVariableReplacement extends Replacement {
+	private Set<String> mergedVariables;
+	
+	public MergeVariableReplacement(Set<String> mergedVariables, String newVariable) {
+		super(mergedVariables.toString(), newVariable, ReplacementType.MERGE_VARIABLES);
+		this.mergedVariables = mergedVariables;
+	}
+
+	public Set<String> getMergedVariables() {
+		return mergedVariables;
+	}
+}

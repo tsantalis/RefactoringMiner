92cb8bc6acff46432f72c34b0c50cdcfc5d6989f
tsantalis <tsantalis@gmail.com>
2019-07-23 12:43:02 -0400

    Introduced composite replacement
    where one statement is matched with multiple statements

---
+++ src/gr/uom/java/xmi/decomposition/replacement/CompositeReplacement.java
@@ -0,0 +1,25 @@
+package gr.uom.java.xmi.decomposition.replacement;
+
+import java.util.Set;
+
+import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
+
+public class CompositeReplacement extends Replacement {
+	private Set<AbstractCodeFragment> additionallyMatchedStatements1;
+	private Set<AbstractCodeFragment> additionallyMatchedStatements2;
+	
+	public CompositeReplacement(String before, String after,
+			Set<AbstractCodeFragment> additionallyMatchedStatements1, Set<AbstractCodeFragment> additionallyMatchedStatements2) {
+		super(before, after, ReplacementType.COMPOSITE);
+		this.additionallyMatchedStatements1 = additionallyMatchedStatements1;
+		this.additionallyMatchedStatements2 = additionallyMatchedStatements2;
+	}
+
+	public Set<AbstractCodeFragment> getAdditionallyMatchedStatements1() {
+		return additionallyMatchedStatements1;
+	}
+
+	public Set<AbstractCodeFragment> getAdditionallyMatchedStatements2() {
+		return additionallyMatchedStatements2;
+	}
+}

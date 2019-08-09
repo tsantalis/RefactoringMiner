f9c61e022c3f1e5dfd3b80735e84d573ae7cf9b5
tsantalis <tsantalis@gmail.com>
2019-08-09 15:31:23 -0400

    Improving condition about excluding common named constants
    from the variable intersection

--- src/gr/uom/java/xmi/decomposition/ReplacementUtil.java
+++ src/gr/uom/java/xmi/decomposition/ReplacementUtil.java
@@ -125,4 +125,27 @@ public class ReplacementUtil {
 		}
 		return false;
 	}
+	
+	public static boolean sameCharsBeforeAfter(String completeString1, String completeString2, String commonSubString) {
+		Pattern p = Pattern.compile(Pattern.quote(commonSubString));
+		Matcher m1 = p.matcher(completeString1);
+		Matcher m2 = p.matcher(completeString2);
+		int matches = 0;
+		int compatibleMatches = 0;
+		while(m1.find() && m2.find()) {
+			int start1 = m1.start();
+			int start2 = m2.start();
+			String characterBeforeMatch1 = start1 == 0 ? "" : String.valueOf(completeString1.charAt(start1 - 1));
+			String characterBeforeMatch2 = start2 == 0 ? "" : String.valueOf(completeString2.charAt(start2 - 1));
+			int end1 = m1.end();
+			int end2 = m2.end();
+			String characterAfterMatch1 = end1 == completeString1.length() ? "" : String.valueOf(completeString1.charAt(end1));
+			String characterAfterMatch2 = end2 == completeString2.length() ? "" : String.valueOf(completeString2.charAt(end2));
+			if(characterBeforeMatch1.equals(characterBeforeMatch2) && characterAfterMatch1.equals(characterAfterMatch2)) {
+				compatibleMatches++;
+			}
+			matches++;
+		}
+		return matches == compatibleMatches;
+	}
 }

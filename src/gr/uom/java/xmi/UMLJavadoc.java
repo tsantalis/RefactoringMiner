054ed1d75f45461d8f65f99de19c712d4c97072c
tsantalis <tsantalis@gmail.com>
2019-08-30 14:31:28 -0400

    Added containsIgnoreCase() in UMLJavadoc to check if javadoc contains
    a string, ignoring the case of the comments and the input

--- src/gr/uom/java/xmi/UMLJavadoc.java
+++ src/gr/uom/java/xmi/UMLJavadoc.java
@@ -17,10 +17,19 @@ public class UMLJavadoc {
 	public List<UMLTagElement> getTags() {
 		return tags;
 	}
-	
+
 	public boolean contains(String s) {
 		for(UMLTagElement tag : tags) {
 			if(tag.contains(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean containsIgnoreCase(String s) {
+		for(UMLTagElement tag : tags) {
+			if(tag.containsIgnoreCase(s)) {
 				return true;
 			}
 		}

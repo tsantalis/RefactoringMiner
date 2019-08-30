483cfc7a1e1743610bb67af998b6388d4e7e5ebf
tsantalis <tsantalis@gmail.com>
2019-08-30 06:27:44 -0400

    addition to previous commit

---
+++ src/gr/uom/java/xmi/UMLJavadoc.java
@@ -0,0 +1,29 @@
+package gr.uom.java.xmi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UMLJavadoc {
+	private List<UMLTagElement> tags;
+
+	public UMLJavadoc() {
+		this.tags = new ArrayList<UMLTagElement>();
+	}
+	
+	public void addTag(UMLTagElement tag) {
+		tags.add(tag);
+	}
+
+	public List<UMLTagElement> getTags() {
+		return tags;
+	}
+	
+	public boolean contains(String s) {
+		for(UMLTagElement tag : tags) {
+			if(tag.contains(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/gr/uom/java/xmi/UMLTagElement.java
@@ -0,0 +1,35 @@
+package gr.uom.java.xmi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UMLTagElement {
+	private String tagName;
+	private List<String> fragments;
+	
+	public UMLTagElement(String tagName) {
+		this.tagName = tagName;
+		this.fragments = new ArrayList<String>();
+	}
+	
+	public void addFragment(String fragment) {
+		fragments.add(fragment);
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public List<String> getFragments() {
+		return fragments;
+	}
+
+	public boolean contains(String s) {
+		for(String fragment : fragments) {
+			if(fragment.contains(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}

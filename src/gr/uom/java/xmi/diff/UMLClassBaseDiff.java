a4fb7f43db095e49b12aa09cdf057e797877e2dd
tsantalis <tsantalis@gmail.com>
2020-02-16 10:35:26 -0500

    Eliminating Rename Variable false positives, when multiple methods are extracted from the same method. This is a fix for false positive Rename Variable runLocal : boolean to stayLocal : boolean in method private ignoreRehashIteration(consumer Consumer<R>, supplier IteratorSupplier<R>, iteratorParallelDistribute boolean) : void in class org.infinispan.stream.impl.DistributedCacheStream in commit https://github.com/infinispan/infinispan/commit/043030723632627b0908dca6b24dae91d3dfd938

--- src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassBaseDiff.java
@@ -1574,9 +1574,44 @@ public abstract class UMLClassBaseDiff implements Comparable<UMLClassBaseDiff> {
 					mapper.addChildMapper(operationBodyMapper);
 					operationsToBeRemoved.add(addedOperation);
 				}
+				checkForInconsistentVariableRenames(mapper);
 			}
 		}
 		addedOperations.removeAll(operationsToBeRemoved);
+	}
+
+	private void checkForInconsistentVariableRenames(UMLOperationBodyMapper mapper) {
+		if(mapper.getChildMappers().size() > 1) {
+			Set<Refactoring> refactoringsToBeRemoved = new LinkedHashSet<Refactoring>();
+			for(Refactoring r : refactorings) {
+				if(r instanceof RenameVariableRefactoring) {
+					RenameVariableRefactoring rename = (RenameVariableRefactoring)r;
+					Set<AbstractCodeMapping> references = rename.getVariableReferences();
+					for(AbstractCodeMapping reference : references) {
+						if(reference.getFragment1().getVariableDeclarations().size() > 0 && !reference.isExact()) {
+							Set<AbstractCodeMapping> allMappingsForReference = new LinkedHashSet<AbstractCodeMapping>();
+							for(UMLOperationBodyMapper childMapper : mapper.getChildMappers()) {
+								for(AbstractCodeMapping mapping : childMapper.getMappings()) {
+									if(mapping.getFragment1().equals(reference.getFragment1())) {
+										allMappingsForReference.add(mapping);
+										break;
+									}
+								}
+							}
+							if(allMappingsForReference.size() > 1) {
+								for(AbstractCodeMapping mapping : allMappingsForReference) {
+									if(!mapping.equals(reference) && mapping.isExact()) {
+										refactoringsToBeRemoved.add(rename);
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			refactorings.removeAll(refactoringsToBeRemoved);
+		}
 	}
 
 	public boolean isEmpty() {

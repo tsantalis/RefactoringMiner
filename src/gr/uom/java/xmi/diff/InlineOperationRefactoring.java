36a8ee0c25edd18d065b3b4532a688785e9b8154
tsantalis <tsantalis@gmail.com>
2020-03-12 19:39:41 -0400

    Fix for issue #87

--- src/gr/uom/java/xmi/diff/InlineOperationRefactoring.java
+++ src/gr/uom/java/xmi/diff/InlineOperationRefactoring.java
@@ -155,6 +155,7 @@ public class InlineOperationRefactoring implements Refactoring {
 
 	public Set<ImmutablePair<String, String>> getInvolvedClassesBeforeRefactoring() {
 		Set<ImmutablePair<String, String>> pairs = new LinkedHashSet<ImmutablePair<String, String>>();
+		pairs.add(new ImmutablePair<String, String>(getInlinedOperation().getLocationInfo().getFilePath(), getInlinedOperation().getClassName()));
 		pairs.add(new ImmutablePair<String, String>(getTargetOperationBeforeInline().getLocationInfo().getFilePath(), getTargetOperationBeforeInline().getClassName()));
 		return pairs;
 	}

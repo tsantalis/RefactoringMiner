fdc74632813ebf16e6f96fc1c744dff375c6cca6
tsantalis <tsantalis@gmail.com>
2020-04-04 19:09:15 -0400

    Taking into account extracted method calls into the body of lambdas
    This improvement enables the detection of refactoring
    Extract Method protected changePreferences(preference CheckBox) : void extracted from protected onCreate(savedInstanceState Bundle) : void in class com.conupods.Settings.SettingsActivity
    in commit
    https://github.com/MinaSelim/SOEN390/commit/9884347034fbd215ff9ddf131233354603f5b9c5

--- src/gr/uom/java/xmi/diff/ExtractOperationDetection.java
+++ src/gr/uom/java/xmi/diff/ExtractOperationDetection.java
@@ -14,6 +14,7 @@ import gr.uom.java.xmi.UMLType;
 import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
 import gr.uom.java.xmi.decomposition.AbstractCodeMapping;
 import gr.uom.java.xmi.decomposition.CompositeStatementObject;
+import gr.uom.java.xmi.decomposition.LambdaExpressionObject;
 import gr.uom.java.xmi.decomposition.OperationInvocation;
 import gr.uom.java.xmi.decomposition.StatementObject;
 import gr.uom.java.xmi.decomposition.UMLOperationBodyMapper;
@@ -139,6 +140,26 @@ public class ExtractOperationDetection {
 			for(OperationInvocation statementInvocation : statementMethodInvocationMap.get(key)) {
 				if(!containsInvocation(operationInvocations, statementInvocation)) {
 					operationInvocations.add(statementInvocation);
+				}
+			}
+		}
+		List<LambdaExpressionObject> lambdas = statement.getLambdas();
+		for(LambdaExpressionObject lambda : lambdas) {
+			if(lambda.getBody() != null) {
+				for(OperationInvocation statementInvocation : lambda.getBody().getAllOperationInvocations()) {
+					if(!containsInvocation(operationInvocations, statementInvocation)) {
+						operationInvocations.add(statementInvocation);
+					}
+				}
+			}
+			if(lambda.getExpression() != null) {
+				Map<String, List<OperationInvocation>> methodInvocationMap = lambda.getExpression().getMethodInvocationMap();
+				for(String key : methodInvocationMap.keySet()) {
+					for(OperationInvocation statementInvocation : methodInvocationMap.get(key)) {
+						if(!containsInvocation(operationInvocations, statementInvocation)) {
+							operationInvocations.add(statementInvocation);
+						}
+					}
 				}
 			}
 		}

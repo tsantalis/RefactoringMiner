d05571ea4c766d1a3ff9999c862d3812cc2edf7e
tsantalis <tsantalis@gmail.com>
2020-08-29 21:39:35 -0400

    Processing enum constant declarations (issue #104)

--- src/gr/uom/java/xmi/UMLClass.java
+++ src/gr/uom/java/xmi/UMLClass.java
@@ -26,6 +26,7 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
     private List<UMLTypeParameter> typeParameters;
     private UMLJavadoc javadoc;
     private List<UMLAnnotation> annotations;
+    private List<UMLEnumConstant> enumConstants;
     
     public UMLClass(String packageName, String name, LocationInfo locationInfo, boolean topLevel, List<String> importedTypes) {
     	super();
@@ -75,6 +76,7 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
         this.importedTypes = importedTypes;
         this.typeParameters = new ArrayList<UMLTypeParameter>();
         this.annotations = new ArrayList<UMLAnnotation>();
+        this.enumConstants = new ArrayList<UMLEnumConstant>();
     }
 
     public List<UMLTypeParameter> getTypeParameters() {
@@ -105,7 +107,15 @@ public class UMLClass extends UMLAbstractClass implements Comparable<UMLClass>, 
     	anonymousClassList.add(anonymousClass);
     }
 
-    public String getPackageName() {
+    public void addEnumConstant(UMLEnumConstant enumConstant) {
+    	enumConstants.add(enumConstant);
+    }
+
+    public List<UMLEnumConstant> getEnumConstants() {
+		return enumConstants;
+	}
+
+	public String getPackageName() {
 		return this.packageName;
 	}
 

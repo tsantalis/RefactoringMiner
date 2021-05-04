edc990b0acfcce6fe5d06678e43026d619328d50
tsantalis <tsantalis@gmail.com>
2021-05-04 16:27:52 -0400

    Moved TypeReplacementAnalysis logic into VariableReplacementAnalysis

--- src/gr/uom/java/xmi/decomposition/VariableReplacementAnalysis.java
+++ src/gr/uom/java/xmi/decomposition/VariableReplacementAnalysis.java
@@ -87,6 +87,56 @@ public class VariableReplacementAnalysis {
 		findConsistentVariableRenames();
 		findParametersWrappedInLocalVariables();
 		findAttributeExtractions();
+		findTypeChanges();
+	}
+
+	private void findTypeChanges() {
+		for(AbstractCodeMapping mapping : mappings) {
+			AbstractCodeFragment fragment1 = mapping.getFragment1();
+			AbstractCodeFragment fragment2 = mapping.getFragment2();
+			for(Replacement replacement : mapping.getReplacements()) {
+				if(replacement.getType().equals(ReplacementType.TYPE)) {
+					List<VariableDeclaration> declarations1 = fragment1.getVariableDeclarations();
+					List<VariableDeclaration> declarations2 = fragment2.getVariableDeclarations();
+					for(VariableDeclaration declaration1 : declarations1) {
+						for(VariableDeclaration declaration2 : declarations2) {
+							if(declaration1.getVariableName().equals(declaration2.getVariableName()) &&
+									(!declaration1.getType().equals(declaration2.getType()) || !declaration1.getType().equalsQualified(declaration2.getType())) &&
+									!containsVariableDeclarationWithSameNameAndType(declaration1, declarations2)) {
+								Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings);
+								getVariableRefactorings(declaration1, declaration2, mapping.getOperation1(), mapping.getOperation2(), variableReferences, null);
+								break;
+							}
+						}
+					}
+				}
+			}
+			if(fragment1.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT) &&
+					fragment2.getLocationInfo().getCodeElementType().equals(CodeElementType.ENHANCED_FOR_STATEMENT)) {
+				List<VariableDeclaration> declarations1 = fragment1.getVariableDeclarations();
+				List<VariableDeclaration> declarations2 = fragment2.getVariableDeclarations();
+				for(VariableDeclaration declaration1 : declarations1) {
+					for(VariableDeclaration declaration2 : declarations2) {
+						if(declaration1.getVariableName().equals(declaration2.getVariableName()) &&
+								(!declaration1.getType().equals(declaration2.getType()) || !declaration1.getType().equalsQualified(declaration2.getType())) &&
+								!containsVariableDeclarationWithSameNameAndType(declaration1, declarations2)) {
+							Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings);
+							getVariableRefactorings(declaration1, declaration2, mapping.getOperation1(), mapping.getOperation2(), variableReferences, null);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private boolean containsVariableDeclarationWithSameNameAndType(VariableDeclaration declaration, List<VariableDeclaration> declarations) {
+		for(VariableDeclaration d : declarations) {
+			if(d.getVariableName().equals(declaration.getVariableName()) && d.getType().equals(declaration.getType()) && d.getType().equalsQualified(declaration.getType())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private void findAttributeExtractions() {

ae8f5d56bb4183485a3055420133552a0a9c02f4
tsantalis <tsantalis@gmail.com>
2021-05-11 07:08:54 -0400

    Added validations for Add Variable/Parameter Modifier (final modifier)

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -13,6 +13,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(9852, 40, 393);
+		test.assertExpectations(10074, 40, 393);
 	}
 }

99dec2364b7ef634a701e1755682fe3e244041b1
tsantalis <tsantalis@gmail.com>
2021-06-23 15:27:12 -0400

    Fixed missing references for _tzFormat type change in commit
    https://github.com/eclipse/jetty.project/commit/0ba1d9b5a567e76596d667136be263d7720a2196#diff-9a41b802328c0d8c6e6e66197865ac4392b5faee2c939d45672322c682b18866L141

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -13,6 +13,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(10471, 36, 386);
+		test.assertExpectations(10472, 36, 385);
 	}
 }

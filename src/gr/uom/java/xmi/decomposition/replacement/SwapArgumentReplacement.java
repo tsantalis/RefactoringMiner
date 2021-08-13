0671bf9caa6d6daba920f72dd4fcfc53bb292da3
tsantalis <tsantalis@gmail.com>
2021-08-13 10:14:00 -0400

    Fix for issue #207

---
+++ src/gr/uom/java/xmi/decomposition/replacement/SwapArgumentReplacement.java
@@ -0,0 +1,9 @@
+package gr.uom.java.xmi.decomposition.replacement;
+
+public class SwapArgumentReplacement extends Replacement {
+
+	public SwapArgumentReplacement(String before, String after) {
+		super(before, after, ReplacementType.SWAP_VARIABLES);
+	}
+
+}

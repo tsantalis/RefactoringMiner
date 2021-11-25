760ae437b953be5f1d42a8a3c121c74d17832eba
tsantalis <tsantalis@gmail.com>
2021-11-25 06:41:02 -0500

    Oracle updates

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -13,6 +13,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(10982, 29, 342);
+		test.assertExpectations(11037, 29, 342);
 	}
 }

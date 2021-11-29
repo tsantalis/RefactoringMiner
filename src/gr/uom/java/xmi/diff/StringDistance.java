36df63857247a7d733724eb0272e66ab1bd35ee3
tsantalis <tsantalis@gmail.com>
2021-11-29 17:03:04 -0500

    Reverting trivialCommentChange() to its previous implementation

--- src/gr/uom/java/xmi/diff/StringDistance.java
+++ src/gr/uom/java/xmi/diff/StringDistance.java
@@ -2,8 +2,8 @@ package gr.uom.java.xmi.diff;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.text.similarity.LevenshteinDistance;
@@ -12,10 +12,9 @@ import com.github.difflib.DiffUtils;
 import com.github.difflib.patch.AbstractDelta;
 import com.github.difflib.patch.Chunk;
 import com.github.difflib.patch.Patch;
-import com.github.difflib.text.DiffRow;
-import com.github.difflib.text.DiffRowGenerator;
 
 public class StringDistance {
+	private static final Pattern COMMENT_LINE = Pattern.compile("^\\s*(//|\\*).*");
 	
 	public static int editDistance(String a, String b, int threshold) {
 		return new LevenshteinDistance(threshold).apply(a, b);
@@ -29,50 +28,12 @@ public class StringDistance {
 		if(fileBefore.length() == fileAfter.length()) {
 			List<String> original = IOUtils.readLines(new StringReader(fileBefore));
 			List<String> revised = IOUtils.readLines(new StringReader(fileAfter));
-			
-			DiffRowGenerator generator = DiffRowGenerator.create()
-	                .showInlineDiffs(true)
-	                .inlineDiffByWord(true)
-	                .build();
-			
+
 			Patch<String> patch = DiffUtils.diff(original, revised);
 			List<AbstractDelta<String>> deltas = patch.getDeltas();
 			for(AbstractDelta<String> delta : deltas) {
 				Chunk<String> source = delta.getSource();
-				Chunk<String> target = delta.getTarget();
-				if(source.getLines().size() == 1 && target.getLines().size() == 1) {
-					List<DiffRow> rows = generator.generateDiffRows(
-							Arrays.asList(source.getLines().get(0)),
-							Arrays.asList(target.getLines().get(0)));
-					if(rows.size() == 1) {
-						String oldLine = rows.get(0).getOldLine();
-						String newLine = rows.get(0).getNewLine();
-						String oldEditStartTag = "<span class=\"editOldInline\">";
-						String newEditStartTag = "<span class=\"editNewInline\">";
-						if(oldLine.contains(oldEditStartTag) && newLine.contains(newEditStartTag)) {
-							String oldValue = oldLine.substring(oldLine.indexOf(oldEditStartTag) + oldEditStartTag.length(), oldLine.indexOf("</span>"));
-							String newValue = newLine.substring(newLine.indexOf(newEditStartTag) + newEditStartTag.length(), newLine.indexOf("</span>"));
-							if(!isNumeric(oldValue) || !isNumeric(newValue)) {
-								return false;
-							}
-						}
-						//removing whitespace
-						else if(oldLine.contains(oldEditStartTag) && newLine.isEmpty()) {
-							String oldValue = oldLine.substring(oldLine.indexOf(oldEditStartTag) + oldEditStartTag.length(), oldLine.indexOf("</span>"));
-							if(!oldValue.isBlank()) {
-								return false;
-							}
-						}
-						//adding whitespace
-						else if(oldLine.isEmpty() && newLine.contains(newEditStartTag)) {
-							String newValue = newLine.substring(newLine.indexOf(newEditStartTag) + newEditStartTag.length(), newLine.indexOf("</span>"));
-							if(!newValue.isBlank()) {
-								return false;
-							}
-						}
-					}
-				}
-				else {
+				if(source.getLines().size() > 0 && !COMMENT_LINE.matcher(source.getLines().get(0)).matches()) {
 					return false;
 				}
 			}

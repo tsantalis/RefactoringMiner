be6c39a858cda05b6fe3f37891f77d20e7937cba
tsantalis <tsantalis@gmail.com>
2021-12-27 10:45:50 -0500

    Enabled the detection of anonymous class extracted to field
    https://github.com/ReactiveX/RxJava/commit/8ad226067434cd39ce493b336bd0659778625959

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -14,6 +14,6 @@ public class TestAllRefactorings extends LightJavaCodeInsightFixtureTestCase {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(11057, 29, 341);
+		test.assertExpectations(11065, 29, 341);
 	}
 }

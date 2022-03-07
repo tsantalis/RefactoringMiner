49d37191ae8d73bd5e77402fb145d54df2bf2ac5
tsantalis <tsantalis@gmail.com>
2022-03-07 14:00:26 -0500

    Improved command line tests to take into account differences in
    start/end column numbers

--- src-test/org/refactoringminer/test/TestCommandLine.java
+++ src-test/org/refactoringminer/test/TestCommandLine.java
@@ -1,16 +1,59 @@
 package org.refactoringminer.test;
 
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Chunk;
+import com.github.difflib.patch.Patch;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.refactoringminer.RefactoringMiner;
+import org.refactoringminer.util.PrefixSuffixUtils;
 
 import java.io.FileReader;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
     private static final String REPOS = "tmp1";
+    private static final Pattern INTEGER = Pattern.compile("-?\\d+");
+
+    private static void assertJSON(List<String> expected, List<String> actual) {
+        Assert.assertEquals(expected.size(), actual.size());
+        Patch<String> patch = DiffUtils.diff(expected, actual);
+        List<AbstractDelta<String>> deltas = patch.getDeltas();
+        int changedLines = 0;
+        int linesWithNumericDifference = 0;
+        for(AbstractDelta<String> delta : deltas) {
+            Chunk<String> source = delta.getSource();
+            Chunk<String> target = delta.getTarget();
+            List<String> sourceLines = source.getLines();
+            List<String> targetLines = target.getLines();
+            if(sourceLines.size() == targetLines.size()) {
+                for(int i = 0; i< sourceLines.size(); i++) {
+                    changedLines++;
+                    String s1 = sourceLines.get(i);
+                    String s2 = targetLines.get(i);
+                    String commonPrefix = PrefixSuffixUtils.longestCommonPrefix(s1, s2);
+                    String commonSuffix = PrefixSuffixUtils.longestCommonSuffix(s1, s2);
+                    if(!commonPrefix.isEmpty() && !commonSuffix.isEmpty()) {
+                        int beginIndexS1 = s1.indexOf(commonPrefix) + commonPrefix.length();
+                        int endIndexS1 = s1.lastIndexOf(commonSuffix);
+                        String diff1 = beginIndexS1 > endIndexS1 ? "" :	s1.substring(beginIndexS1, endIndexS1);
+                        int beginIndexS2 = s2.indexOf(commonPrefix) + commonPrefix.length();
+                        int endIndexS2 = s2.lastIndexOf(commonSuffix);
+                        String diff2 = beginIndexS2 > endIndexS2 ? "" :	s2.substring(beginIndexS2, endIndexS2);
+                        if(!diff1.isEmpty() && INTEGER.matcher(diff1).matches() && !diff2.isEmpty() && INTEGER.matcher(diff2).matches()) {
+                            linesWithNumericDifference++;
+                        }
+                    }
+                }
+            }
+        }
+        Assert.assertEquals(changedLines, linesWithNumericDifference);
+    }
+
     @Test
     public void testBetweenCommits() throws Exception {
         RefactoringMiner miner = new RefactoringMiner(getProject());
@@ -27,7 +70,7 @@ public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
 
         List<String> expected = IOUtils.readLines(new FileReader(System.getProperty("user.dir") + "/src-test/Data/mondrian-bc-expected.json"));
         List<String> actual = IOUtils.readLines(new FileReader(jsonPath));
-        Assert.assertEquals(expected.size(), actual.size());
+        assertJSON(expected, actual);
     }
 
     @Test
@@ -46,7 +89,7 @@ public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
 
         List<String> expected = IOUtils.readLines(new FileReader(System.getProperty("user.dir") + "/src-test/Data/mondrian-bt-expected.json"));
         List<String> actual = IOUtils.readLines(new FileReader(jsonPath));
-        Assert.assertEquals(expected.size(), actual.size());
+        assertJSON(expected, actual);
     }
 
     @Test
@@ -63,7 +106,7 @@ public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
 
         List<String> expected = IOUtils.readLines(new FileReader(System.getProperty("user.dir") + "/src-test/Data/refactoring-toy-example-all-expected.json"));
         List<String> actual = IOUtils.readLines(new FileReader(jsonPath));
-        Assert.assertEquals(expected.size(), actual.size());
+        assertJSON(expected, actual);
     }
 
     @Test
@@ -81,7 +124,7 @@ public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
 
         List<String> expected = IOUtils.readLines(new FileReader(System.getProperty("user.dir") + "/src-test/Data/refactoring-toy-example-branch-expected.json"));
         List<String> actual = IOUtils.readLines(new FileReader(jsonPath));
-        Assert.assertEquals(expected.size(), actual.size());
+        assertJSON(expected, actual);
     }
 
     @Test
@@ -99,6 +142,6 @@ public class TestCommandLine extends LightJavaCodeInsightFixtureTestCase {
 
         List<String> expected = IOUtils.readLines(new FileReader(System.getProperty("user.dir") + "/src-test/Data/refactoring-toy-example-commit-expected.json"));
         List<String> actual = IOUtils.readLines(new FileReader(jsonPath));
-        Assert.assertEquals(expected.size(), actual.size());
+        assertJSON(expected, actual);
     }
 }

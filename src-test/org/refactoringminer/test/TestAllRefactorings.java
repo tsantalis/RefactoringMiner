e6ee70639aea838f2ebd85b8b15db31b5db32161
tsantalis <tsantalis@gmail.com>
2022-03-30 13:33:09 -0400

    Finalized changes from commit 2d33061c19329dca360ae1fa6e4c156a1878ad80

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -13,6 +13,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(11134, 29, 319);
+		test.assertExpectations(11135, 29, 319);
 	}
 }

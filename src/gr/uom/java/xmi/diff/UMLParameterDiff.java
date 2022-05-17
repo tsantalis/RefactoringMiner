c9a25533370f956fb2e88df63c8de31011dc198c
tsantalis <tsantalis@gmail.com>
2022-05-16 20:50:04 -0400

    Added common annotations in UMLAnnotationDiffList

--- src/gr/uom/java/xmi/diff/UMLParameterDiff.java
+++ src/gr/uom/java/xmi/diff/UMLParameterDiff.java
@@ -98,7 +98,7 @@ public class UMLParameterDiff {
 		for(UMLAnnotation annotation : annotationListDiff.getAddedAnnotations()) {
 			sb.append("\t").append("annotation " + annotation + " added").append("\n");
 		}
-		for(UMLAnnotationDiff annotationDiff : annotationListDiff.getAnnotationDiffList()) {
+		for(UMLAnnotationDiff annotationDiff : annotationListDiff.getAnnotationDiffs()) {
 			sb.append("\t").append("annotation " + annotationDiff.getRemovedAnnotation() + " modified to " + annotationDiff.getAddedAnnotation()).append("\n");
 		}
 		return sb.toString();
@@ -137,7 +137,7 @@ public class UMLParameterDiff {
 			RemoveVariableAnnotationRefactoring refactoring = new RemoveVariableAnnotationRefactoring(annotation, originalVariable, newVariable, removedOperation, addedOperation, false);
 			refactorings.add(refactoring);
 		}
-		for(UMLAnnotationDiff annotationDiff : annotationListDiff.getAnnotationDiffList()) {
+		for(UMLAnnotationDiff annotationDiff : annotationListDiff.getAnnotationDiffs()) {
 			ModifyVariableAnnotationRefactoring refactoring = new ModifyVariableAnnotationRefactoring(annotationDiff.getRemovedAnnotation(), annotationDiff.getAddedAnnotation(), originalVariable, newVariable, removedOperation, addedOperation, false);
 			refactorings.add(refactoring);
 		}

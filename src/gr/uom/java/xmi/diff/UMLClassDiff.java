7e0471ce6e9960475cc510c11caed092dff669ce
tsantalis <tsantalis@gmail.com>
2022-08-17 10:53:54 -0400

    Avoid the overriding of processAnonymousClasses()
    in UMLClassDiff, as it depends on UMLAnonymousClass.equals()
    The implementation in UMLClassBaseDiff is the correct one.
    Case study:
    https://github.com/junit-team/junit4/commit/3f736cf514b0421a82383db74188f6c0e3dfef1a#diff-cd3bb9170f8d6404c5dc5bb66f7b647435854d48fe357e0bd998e3011a575929R95
    Update version 2.3.2
    Removing dependency to fr.jmini.ecentral:eclipse-platform-dependencies

--- src/gr/uom/java/xmi/diff/UMLClassDiff.java
+++ src/gr/uom/java/xmi/diff/UMLClassDiff.java
@@ -110,17 +110,6 @@ public class UMLClassDiff extends UMLClassBaseDiff {
 		return false;
 	}
 
-	protected void processAnonymousClasses() {
-		for(UMLAnonymousClass umlAnonymousClass : originalClass.getAnonymousClassList()) {
-    		if(!nextClass.getAnonymousClassList().contains(umlAnonymousClass))
-    			this.reportRemovedAnonymousClass(umlAnonymousClass);
-    	}
-    	for(UMLAnonymousClass umlAnonymousClass : nextClass.getAnonymousClassList()) {
-    		if(!originalClass.getAnonymousClassList().contains(umlAnonymousClass))
-    			this.reportAddedAnonymousClass(umlAnonymousClass);
-    	}
-	}
-
 	protected void createBodyMappers() throws RefactoringMinerTimedOutException {
 		for(UMLOperation originalOperation : originalClass.getOperations()) {
 			for(UMLOperation nextOperation : nextClass.getOperations()) {

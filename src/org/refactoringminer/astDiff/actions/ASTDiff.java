591e6f014555d4a35d1fa320ce1d3b6f72c11a2b
pouryafard75 <pouryafard75@gmail.com>
2022-12-28 16:02:07 -0500

    More infrastructure for AST diff

---
+++ src/org/refactoringminer/astDiff/actions/ASTDiff.java
@@ -0,0 +1,62 @@
+package org.refactoringminer.astDiff.actions;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.refactoringminer.astDiff.matchers.ExtendedMultiMappingStore;
+
+import com.github.gumtreediff.actions.Diff;
+import com.github.gumtreediff.actions.EditScript;
+import com.github.gumtreediff.actions.TreeClassifier;
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.tree.Tree;
+import com.github.gumtreediff.tree.TreeContext;
+
+/**
+ * @author  Pourya Alikhani Fard pouryafard75@gmail.com
+ */
+public class ASTDiff extends Diff {
+	private ExtendedMultiMappingStore multiMappings;
+
+	public ASTDiff(TreeContext src, TreeContext dst, ExtendedMultiMappingStore mappings,
+			Map<String, TreeContext> parentContextMap, Map<String, TreeContext> childContextMap) {
+		super(src, dst, mappings.getMonoMappingStore(), computeEditScript(mappings, parentContextMap, childContextMap));
+		this.multiMappings = mappings;
+	}
+
+	public ExtendedMultiMappingStore getMultiMappings() {
+		return multiMappings;
+	}
+
+	private static EditScript computeEditScript(ExtendedMultiMappingStore mappings, 
+			Map<String, TreeContext> parentContextMap, Map<String, TreeContext> childContextMap) {
+		EditScript editScript = new SimplifiedChawatheScriptGenerator().computeActions(mappings,parentContextMap,childContextMap);
+		processMultiMaps(mappings, editScript);
+		return editScript;
+	}
+
+	private static void processMultiMaps(ExtendedMultiMappingStore mappings, EditScript editScript) {
+		//ArrayList<Action> multiMoves = new ArrayList<>();
+		Map<Tree, Set<Tree>> dstToSrcMultis = mappings.dstToSrcMultis();
+		MultiMoveActionGenerator multiMoveActionGenerator = new MultiMoveActionGenerator();
+
+		for(Map.Entry<Tree, Set<Tree>> entry : dstToSrcMultis.entrySet())
+		{
+			Set<Tree> srcTrees = entry.getValue();
+			Set<Tree> dstTrees = mappings.getDsts(srcTrees.iterator().next());
+			multiMoveActionGenerator.addMapping(srcTrees,dstTrees);
+		}
+		for (Action action : multiMoveActionGenerator.generate()) {
+			editScript.add(action);
+		}
+	}
+
+	/**
+	 * Compute and return a root node classifier that indicates which node have
+	 * been added/deleted/updated/moved. Only the root note is marked when a whole
+	 * subtree has been subject to a same operation.
+	 */
+	public TreeClassifier createRootNodesClassifier() {
+		return new ExtendedOnlyRootsClassifier(this);
+	}
+}

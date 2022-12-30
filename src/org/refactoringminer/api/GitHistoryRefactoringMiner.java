4031d10127d354523ccacaa45878f294880dd219
tsantalis <tsantalis@gmail.com>
2022-12-30 11:50:05 -0500

    Removed churnAtCommit API

--- src/org/refactoringminer/api/GitHistoryRefactoringMiner.java
+++ src/org/refactoringminer/api/GitHistoryRefactoringMiner.java
@@ -122,15 +122,6 @@ public interface GitHistoryRefactoringMiner {
 	void detectAtDirectories(File previousDirectory, File nextDirectory, RefactoringHandler handler);
 
 	/**
-	 * Detect refactorings performed in the specified commit. 
-	 * 
-	 * @param repository A git repository (from JGit library).
-	 * @param commitId The SHA key that identifies the commit.
-	 * @param handler A handler object that is responsible to process the detected refactorings. 
-	 */
-	Churn churnAtCommit(Repository repository, String commitId, RefactoringHandler handler);
-
-	/**
 	 * @return An ID that represents the current configuration for the Refactoring Miner algorithm in use.
 	 */
 	String getConfigId();

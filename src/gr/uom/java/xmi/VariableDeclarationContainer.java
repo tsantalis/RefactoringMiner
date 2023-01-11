cdba96529b4cf9c095f862be7ffb887d61563bcc
tsantalis <tsantalis@gmail.com>
2023-01-10 20:20:53 -0500

    Improvement on issue #323
    Handles cases:
    https://github.com/javaparser/javaparser/commit/a25f53f8871fd178b6791d1194d7358b55d1ba37
    https://github.com/wordpress-mobile/WordPress-Android/commit/1b21ba4bcea986988d4bbd578e3bb9a20ec69606

--- src/gr/uom/java/xmi/VariableDeclarationContainer.java
+++ src/gr/uom/java/xmi/VariableDeclarationContainer.java
@@ -184,14 +184,6 @@ public interface VariableDeclarationContainer extends LocationInfoProvider {
 					keysToBeRemoved.add(key);
 				}
 				else {
-					//exclude exception variables declared in catch blocks
-					for(VariableDeclaration variable : variableDeclarationMap.get(key)) {
-						UMLType variableType = variable.getType();
-						if(variableType != null && variableType.getClassType().endsWith("Exception")) {
-							keysToBeRemoved.add(key);
-							break;
-						}
-					}
 					//exclude variables aliased with fields
 					boolean foundInLocalVariables = false;
 					for(String value : map.get(key)) {

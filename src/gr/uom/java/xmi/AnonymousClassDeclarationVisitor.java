0a4d2140b4b827bcd1e2632ead470f132d6df95c
tsantalis <tsantalis@gmail.com>
2023-01-23 19:48:53 -0500

    Support for TypeDeclarationStatement
    Fixes
    https://github.com/pouryafard75/RM-ASTDiff/issues/4
    https://github.com/pouryafard75/RM-ASTDiff/issues/18

--- src/gr/uom/java/xmi/AnonymousClassDeclarationVisitor.java
+++ src/gr/uom/java/xmi/AnonymousClassDeclarationVisitor.java
@@ -5,13 +5,25 @@ import java.util.Set;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclarationStatement;
 
 public class AnonymousClassDeclarationVisitor extends ASTVisitor {
 
-	private Set<AnonymousClassDeclaration> anonymousClassDeclarations = new LinkedHashSet<AnonymousClassDeclaration>();
+	private Set<AnonymousClassDeclaration> anonymousClassDeclarations = new LinkedHashSet<>();
+	private Set<TypeDeclarationStatement> typeDeclarationStatements = new LinkedHashSet<>();
+	
+	public boolean visit(TypeDeclarationStatement node) {
+		typeDeclarationStatements.add(node);
+		return super.visit(node);
+	}
+	
 	public boolean visit(AnonymousClassDeclaration node) {
 		anonymousClassDeclarations.add(node);
 		return super.visit(node);
+	}
+
+	public Set<TypeDeclarationStatement> getTypeDeclarationStatements() {
+		return typeDeclarationStatements;
 	}
 
 	public Set<AnonymousClassDeclaration> getAnonymousClassDeclarations() {

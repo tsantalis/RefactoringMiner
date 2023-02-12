f33148b2683b2ec607b614c0235f9eed143cbb20
tsantalis <tsantalis@gmail.com>
2023-02-12 10:09:33 -0500

    Fix for issue #359
    Support the replacement of attributes with local variables (and vice-versa) having the same name and type.

--- src/gr/uom/java/xmi/decomposition/VariableReplacementAnalysis.java
+++ src/gr/uom/java/xmi/decomposition/VariableReplacementAnalysis.java
@@ -653,12 +653,43 @@ public class VariableReplacementAnalysis {
 					}
 				}
 			}
+			for(AbstractCodeFragment fragment1 : mapper.getNonMappedLeavesT1()) {
+				for(VariableDeclaration declaration1 : fragment1.getVariableDeclarations()) {
+					for(UMLAttribute addedAttribute : addedAttributes) {
+						VariableDeclaration declaration2 = addedAttribute.getVariableDeclaration();
+						if(declaration1.getVariableName().equals(declaration2.getVariableName()) && declaration1.equalType(declaration2)) {
+							Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings, classDiff, modelDiff);
+							if(variableReferences.size() > 0) {
+								RenameVariableRefactoring refactoring = new RenameVariableRefactoring(declaration1, declaration2, operation1, operation2, variableReferences, insideExtractedOrInlinedMethod);
+								refactorings.add(refactoring);
+								break;
+							}
+						}
+					}
+				}
+			}
+			for(AbstractCodeFragment fragment2 : mapper.getNonMappedLeavesT2()) {
+				for(VariableDeclaration declaration2 : fragment2.getVariableDeclarations()) {
+					for(UMLAttribute removedAttribute : removedAttributes) {
+						VariableDeclaration declaration1 = removedAttribute.getVariableDeclaration();
+						if(declaration1.getVariableName().equals(declaration2.getVariableName()) && declaration1.equalType(declaration2)) {
+							Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings, classDiff, modelDiff);
+							if(variableReferences.size() > 0) {
+								RenameVariableRefactoring refactoring = new RenameVariableRefactoring(declaration1, declaration2, operation1, operation2, variableReferences, insideExtractedOrInlinedMethod);
+								refactorings.add(refactoring);
+								break;
+							}
+						}
+					}
+				}
+			}
+			Set<AbstractCodeMapping> mappingsToBeRemoved = new LinkedHashSet<>();
 			for(AbstractCodeMapping mapping : mappings) {
 				for(Replacement replacement : mapping.getReplacements()) {
 					if(replacement.involvesVariable()) {
 						for(UMLAttribute addedAttribute : addedAttributes) {
-							VariableDeclaration variableDeclaration = addedAttribute.getVariableDeclaration();
-							if(addedAttribute.getName().equals(replacement.getAfter()) && extractInlineCondition(variableDeclaration, replacement, replacement.getBefore())) {
+							VariableDeclaration declaration2 = addedAttribute.getVariableDeclaration();
+							if(addedAttribute.getName().equals(replacement.getAfter()) && extractInlineCondition(declaration2, replacement, replacement.getBefore())) {
 								ExtractAttributeRefactoring refactoring = new ExtractAttributeRefactoring(addedAttribute, classDiff.getOriginalClass(), classDiff.getNextClass(), insideExtractedOrInlinedMethod);
 								if(refactorings.contains(refactoring)) {
 									Iterator<Refactoring> it = refactorings.iterator();
@@ -675,10 +706,23 @@ public class VariableReplacementAnalysis {
 									refactorings.add(refactoring);
 								}
 							}
+							else {
+								for(VariableDeclaration declaration1 : mapping.getFragment1().getVariableDeclarations()) {
+									if(declaration1.getVariableName().equals(declaration2.getVariableName()) && declaration1.equalType(declaration2)) {
+										Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings, classDiff, modelDiff);
+										if(variableReferences.size() > 0 && mapping.getFragment2().getVariableDeclaration(declaration1.getVariableName()) == null) {
+											RenameVariableRefactoring refactoring = new RenameVariableRefactoring(declaration1, declaration2, operation1, operation2, variableReferences, insideExtractedOrInlinedMethod);
+											refactorings.add(refactoring);
+											mappingsToBeRemoved.add(mapping);
+											break;
+										}
+									}
+								}
+							}
 						}
 						for(UMLAttribute removedAttribute : removedAttributes) {
-							VariableDeclaration variableDeclaration = removedAttribute.getVariableDeclaration();
-							if(removedAttribute.getName().equals(replacement.getBefore()) && extractInlineCondition(variableDeclaration, replacement, replacement.getAfter())) {
+							VariableDeclaration declaration1 = removedAttribute.getVariableDeclaration();
+							if(removedAttribute.getName().equals(replacement.getBefore()) && extractInlineCondition(declaration1, replacement, replacement.getAfter())) {
 								InlineAttributeRefactoring refactoring = new InlineAttributeRefactoring(removedAttribute, classDiff.getOriginalClass(), classDiff.getNextClass(), insideExtractedOrInlinedMethod);
 								if(refactorings.contains(refactoring)) {
 									Iterator<Refactoring> it = refactorings.iterator();
@@ -695,9 +739,25 @@ public class VariableReplacementAnalysis {
 									refactorings.add(refactoring);
 								}
 							}
+							else {
+								for(VariableDeclaration declaration2 : mapping.getFragment2().getVariableDeclarations()) {
+									if(declaration1.getVariableName().equals(declaration2.getVariableName()) && declaration1.equalType(declaration2)) {
+										Set<AbstractCodeMapping> variableReferences = VariableReferenceExtractor.findReferences(declaration1, declaration2, mappings, classDiff, modelDiff);
+										if(variableReferences.size() > 0 && mapping.getFragment1().getVariableDeclaration(declaration2.getVariableName()) == null) {
+											RenameVariableRefactoring refactoring = new RenameVariableRefactoring(declaration1, declaration2, operation1, operation2, variableReferences, insideExtractedOrInlinedMethod);
+											refactorings.add(refactoring);
+											mappingsToBeRemoved.add(mapping);
+											break;
+										}
+									}
+								}
+							}
 						}
 					}
 				}
+			}
+			for(AbstractCodeMapping mapping : mappingsToBeRemoved) {
+				mapper.removeMapping(mapping);
 			}
 		}
 	}

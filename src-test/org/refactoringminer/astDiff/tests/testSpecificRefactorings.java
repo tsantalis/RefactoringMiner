0699a3c900179793c2f54c1d489642daf971b3eb
Pourya Fard <pouryafard75@gmail.com>
2023-03-22 17:49:43 -0400

    Test for not matching the return statement inside the ExtractMethod

--- src-test/org/refactoringminer/astDiff/tests/testSpecificRefactorings.java
+++ src-test/org/refactoringminer/astDiff/tests/testSpecificRefactorings.java
@@ -1,13 +1,16 @@
 package org.refactoringminer.astDiff.tests;
 
 import com.github.gumtreediff.matchers.Mapping;
+import gui.webdiff.WebDiff;
 import org.junit.Test;
 import org.refactoringminer.astDiff.actions.ASTDiff;
+import org.refactoringminer.astDiff.matchers.Constants;
 import org.refactoringminer.astDiff.utils.URLHelper;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
 
 import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /* Created by pourya on 2023-02-28 4:48 p.m. */
@@ -39,6 +42,26 @@ public class testSpecificRefactorings {
             assertTrue("SimpleName For RenameParameter Refactoring ",m3Check);
         }
 
+    }
+    @Test
+    public void testExtractMethodReturnStatement() {
+        String returnTreeSrc = "ReturnStatement [17511,17714]";
+        String url = "https://github.com/ReactiveX/RxJava/commit/8ad226067434cd39ce493b336bd0659778625959";
+        String repo = URLHelper.getRepo(url);
+        String commit = URLHelper.getCommit(url);
+        Set<ASTDiff> astDiffs = new GitHistoryRefactoringMinerImpl().diffAtCommit(repo, commit, 1000);
+        for (ASTDiff astDiff : astDiffs) {
+            if (!astDiff.getSrcPath().equals("src/test/java/rx/observables/BlockingObservableTest.java"))
+                continue;
+            Set<Mapping> mappings = astDiff.getMultiMappings().getMappings();
+            int numOfMappingsForReturnSubTree = 0;
+            int numOfMappingsForStatementSubTreeContainingReturn = 0;
+            for (Mapping mapping : mappings) {
+                if (mapping.first.toString().equals(returnTreeSrc))
+                    numOfMappingsForReturnSubTree += 1;
+            }
+            assertEquals(String.format("Number of mappings for %s not equal to 1", returnTreeSrc), 1, numOfMappingsForReturnSubTree);
+        }
     }
 
 }

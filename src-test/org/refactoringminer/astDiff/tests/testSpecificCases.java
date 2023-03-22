57813d6777e9e49e85a4fa75853730d1cfa9da04
Pouryafard75 <pouryafard75@gmail.com>
2023-03-22 18:28:51 -0400

    Improved test assertions

--- src-test/org/refactoringminer/astDiff/tests/testSpecificCases.java
+++ src-test/org/refactoringminer/astDiff/tests/testSpecificCases.java
@@ -1,10 +1,8 @@
 package org.refactoringminer.astDiff.tests;
 
 import com.github.gumtreediff.matchers.Mapping;
-import gui.webdiff.WebDiff;
 import org.junit.Test;
 import org.refactoringminer.astDiff.actions.ASTDiff;
-import org.refactoringminer.astDiff.matchers.Constants;
 import org.refactoringminer.astDiff.utils.URLHelper;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
 
@@ -14,11 +12,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /* Created by pourya on 2023-02-28 4:48 p.m. */
-public class testSpecificRefactorings {
+public class testSpecificCases {
 
     @Test
     public void testRenameParameter() {
-
         String m1 = "SingleVariableDeclaration [3886,3897] -> SingleVariableDeclaration [2778,2797]";
         String m2 = "PrimitiveType: long [3886,3890] -> PrimitiveType: long [2778,2782]";
         String m3 = "SimpleName: millis [3891,3897] -> SimpleName: durationMillis [2783,2797]";
@@ -26,6 +23,7 @@ public class testSpecificRefactorings {
         String repo = URLHelper.getRepo(url);
         String commit = URLHelper.getCommit(url);
         Set<ASTDiff> astDiffs = new GitHistoryRefactoringMinerImpl().diffAtCommit(repo, commit, 1000);
+        boolean executed = false;
         for (ASTDiff astDiff : astDiffs) {
             System.out.println();
             if (!astDiff.getSrcPath().equals("src/java/org/apache/commons/lang/time/DurationFormatUtils.java"))
@@ -40,7 +38,9 @@ public class testSpecificRefactorings {
             assertTrue("SingleVariableDeclaration For RenameParameter Refactoring ",m1Check);
             assertTrue("PrimitiveType Long For RenameParameter Refactoring ",m2Check);
             assertTrue("SimpleName For RenameParameter Refactoring ",m3Check);
+            executed = true;
         }
+        assertTrue("RenameParameter test case not executed properly", executed);
 
     }
     @Test
@@ -50,18 +50,21 @@ public class testSpecificRefactorings {
         String repo = URLHelper.getRepo(url);
         String commit = URLHelper.getCommit(url);
         Set<ASTDiff> astDiffs = new GitHistoryRefactoringMinerImpl().diffAtCommit(repo, commit, 1000);
+        boolean executed = false;
         for (ASTDiff astDiff : astDiffs) {
             if (!astDiff.getSrcPath().equals("src/test/java/rx/observables/BlockingObservableTest.java"))
                 continue;
             Set<Mapping> mappings = astDiff.getMultiMappings().getMappings();
             int numOfMappingsForReturnSubTree = 0;
-            int numOfMappingsForStatementSubTreeContainingReturn = 0;
             for (Mapping mapping : mappings) {
                 if (mapping.first.toString().equals(returnTreeSrc))
                     numOfMappingsForReturnSubTree += 1;
             }
+            executed = true;
             assertEquals(String.format("Number of mappings for %s not equal to 1", returnTreeSrc), 1, numOfMappingsForReturnSubTree);
         }
+        assertTrue("ExtractMethodReturnStatement not executed properly", executed);
+
     }
 
 }

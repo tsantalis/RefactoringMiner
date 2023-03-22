619bfcc02c25a4c61696bac623ebb0be505b790c
tsantalis <tsantalis@gmail.com>
2023-03-22 14:40:31 -0400

    Fix for issue #415

--- src-test/org/refactoringminer/test/TestAllRefactorings.java
+++ src-test/org/refactoringminer/test/TestAllRefactorings.java
@@ -13,6 +13,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, "tmp1", Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectations(11600, 23, 275);
+		test.assertExpectations(11602, 23, 273);
 	}
 }

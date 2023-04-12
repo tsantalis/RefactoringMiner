3a09aaa58f9b7815ee16de841c5c421437a9173b
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-04-12 16:47:24 -0400

    Migrate AllCasesWithLocallyClonedReposTest to JUnit 5 ParameterizedTest with MethodSource
    Co-authored-by: Pourya Fard <pouryafard75@gmail.com>

--- src-test/org/refactoringminer/astDiff/tests/AllCasesWithLocallyClonedReposTest.java
+++ src-test/org/refactoringminer/astDiff/tests/AllCasesWithLocallyClonedReposTest.java
@@ -3,9 +3,9 @@ package org.refactoringminer.astDiff.tests;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jgit.lib.Repository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.refactoringminer.api.GitService;
 import org.refactoringminer.astDiff.actions.ASTDiff;
 import org.refactoringminer.astDiff.utils.CaseInfo;
@@ -14,6 +14,7 @@ import org.refactoringminer.util.GitServiceImpl;
 
 import java.io.File;
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.refactoringminer.astDiff.utils.UtilMethods.*;
@@ -22,32 +23,19 @@ import static org.refactoringminer.astDiff.utils.UtilMethods.*;
  * @author  Pourya Alikhani Fard pouryafard75@gmail.com
  */
 
-@RunWith(Parameterized.class)
 public class AllCasesWithLocallyClonedReposTest {
 	private static final String REPOS = "tmp1";
 	private static GitService gitService = new GitServiceImpl();
-	
-    @Parameterized.Parameter(0)
-    public String repo;
-    @Parameterized.Parameter(1)
-    public String commit;
-    @Parameterized.Parameter(2)
-    public String srcFileName;
-    @Parameterized.Parameter(3)
-    public String expected;
-    @Parameterized.Parameter(4)
-    public String actual;
-
-    @Test
-    public void testChecker() {
+    @ParameterizedTest(name= "{index}: File: {2}, Repo: {0}, Commit: {1}")
+    @MethodSource("initDataWithClonedRepos")
+    public void testChecker(String repo, String commit, String srcFileName, String expected, String actual) {
         String msg = String.format("Failed for %s/commit/%s , srcFileName: %s",repo.replace(".git",""),commit,srcFileName);
         assertEquals(msg, expected.length(),actual.length());
         assertEquals(msg, expected,actual);
     }
 
-    @Parameterized.Parameters(name= "{index}: File: {2}, Repo: {0}, Commit: {1}")
-    public static Iterable<Object[]> initDataWithClonedRepos() throws Exception {
-        List<Object[]> allCases = new ArrayList<>();
+    public static Stream<Arguments> initDataWithClonedRepos() throws Exception {
+        List<Arguments> allCases = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         String jsonFile = getCommitsMappingsPath() + getTestInfoFile();
         List<CaseInfo> infos = mapper.readValue(new File(jsonFile), new TypeReference<List<CaseInfo>>(){});
@@ -60,12 +48,11 @@ public class AllCasesWithLocallyClonedReposTest {
 
             makeAllCases(allCases, info, expectedFilesList, astDiffs);
         }
-        return allCases;
+        return allCases.stream();
     }
 
-//    @Parameterized.Parameters(name= "{index}: File: {2}, Repo: {0}, Commit: {1}")
-    public static Iterable<Object[]> initDataWithGithub() throws Exception {
-        List<Object[]> allCases = new ArrayList<>();
+    public static Stream<Arguments> initDataWithGithub() throws Exception {
+        List<Arguments> allCases = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         String jsonFile = getCommitsMappingsPath() + getTestInfoFile();
         List<CaseInfo> infos = mapper.readValue(new File(jsonFile), new TypeReference<List<CaseInfo>>(){});
@@ -76,6 +63,6 @@ public class AllCasesWithLocallyClonedReposTest {
 
             makeAllCases(allCases, info, expectedFilesList, astDiffs);
         }
-        return allCases;
+        return allCases.stream();
     }
 }

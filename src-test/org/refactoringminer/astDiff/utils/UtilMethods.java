4cd7b9193a2858b257c1bb43ebe6c32a805fcd68
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-04-12 16:47:24 -0400

    Migrate AllCasesWithLocallyClonedReposTest to JUnit 5 ParameterizedTest with MethodSource
    Co-authored-by: Pourya Fard <pouryafard75@gmail.com>

--- src-test/org/refactoringminer/astDiff/utils/UtilMethods.java
+++ src-test/org/refactoringminer/astDiff/utils/UtilMethods.java
@@ -1,6 +1,7 @@
 package org.refactoringminer.astDiff.utils;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.params.provider.Arguments;
 import org.refactoringminer.astDiff.actions.ASTDiff;
 
 import java.io.File;
@@ -50,20 +51,20 @@ public class UtilMethods {
         return infoFile;
     }
 
-    public static void makeAllCases(List<Object[]> allCases, CaseInfo info, List<String> expectedFilesList, Set<ASTDiff> astDiffs) throws IOException {
+    public static void makeAllCases(List<Arguments> allCases, CaseInfo info, List<String> expectedFilesList, Set<ASTDiff> astDiffs) throws IOException {
         for (ASTDiff astDiff : astDiffs) {
             String finalFilePath = getFinalFilePath(astDiff, getCommitsMappingsPath(), info.getRepo(), info.getCommit());
             String calculated = MappingExportModel.exportString(astDiff.getMultiMappings());
             String expected = FileUtils.readFileToString(new File(finalFilePath), "utf-8");
-            allCases.add(new Object[]{info.getRepo(),info.getCommit(),astDiff.getSrcPath(),expected,calculated});
+            allCases.add(Arguments.of(info.getRepo(),info.getCommit(),astDiff.getSrcPath(),expected,calculated));
             expectedFilesList.remove(getFileNameFromSrcDiff(astDiff.getSrcPath()));
         }
         for (String expectedButNotGeneratedFile : expectedFilesList) {
             String expectedDiffName = getSrcASTDiffFromFile(expectedButNotGeneratedFile);
-            allCases.add(new Object[]
-                    {
+            allCases.add(Arguments.of
+                    (
                     info.getRepo(),info.getCommit(),expectedDiffName,"{JSON}","NOT GENERATED"
-                    }
+                    )
             );
         }
     }

f5afe7dcc1cb7758664536b2769a2befc1a0b475
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-06-08 18:36:43 -0400

    Add failing tests covering partially implemented ParameterizedTest Sources (Empty, Null, NullAndEmpty, Value, and Enum)

--- src-test/gr/uom/java/xmi/diff/UMLClassBaseDiffTest.java
+++ src-test/gr/uom/java/xmi/diff/UMLClassBaseDiffTest.java
@@ -23,11 +23,16 @@ import org.refactoringminer.api.RefactoringMinerTimedOutException;
 import org.refactoringminer.api.RefactoringType;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.InvalidParameterException;
+import java.time.Clock;
+import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -47,6 +52,333 @@ class UMLClassBaseDiffTest {
     }
     static boolean isVictorParameterizedTestDisabled() {
         return !UMLClassBaseDiff.ENABLE_VICTOR_PARAMETERIZED_TEST_DETECTION;
+    }
+    @EnabledIf("isVictorParameterizedTestDisabled")
+    @ParameterizedTest
+    @ValueSource(strings = {"""
+                public class TestClass {
+                    @Test
+                    void testTestFileRelativePath() {
+                        assertNotNull("");
+                        assertTrue("".length() >= 0);
+                        assertTrue("".length() < 2);
+                    }
+                    @Test
+                    void testTestFileRelativePath_1() {
+                        assertNotNull("1");
+                        assertTrue("1".length() >= 0);
+                        assertTrue("1".length() < 2);
+                    }
+                    @Test
+                    void testTestFileRelativePath_2() {
+                        assertNotNull("2");
+                        assertTrue("2".length() >= 0);
+                        assertTrue("2".length() < 2);
+                    }
+                    @Test
+                    void testTestFileRelativePath_3() {
+                        assertNotNull("3");
+                        assertTrue("3".length() >= 0);
+                        assertTrue("3".length() < 2);
+                    }
+                    @Test
+                    void testTestFileRelativePath_4() {
+                        assertNotNull("4");
+                        assertTrue("4".length() >= 0);
+                        assertTrue("4".length() < 2);
+                    }
+                    @Test
+                    void testTestFileRelativePath_5() {
+                        assertNotNull("5");
+                        assertTrue("5".length() >= 0);
+                        assertTrue("5".length() < 2);
+                    }
+                }
+                """,
+                """
+                    public class TestClass {
+                        @Test
+                        void testTestFileRelativePath() {
+                            String s = "";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                        @Test
+                        void testTestFileRelativePath_1() {
+                            String s = "1";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                        @Test
+                        void testTestFileRelativePath_2() {
+                            String s = "2";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                        @Test
+                        void testTestFileRelativePath_3() {
+                            String s = "3";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                        @Test
+                        void testTestFileRelativePath_4() {
+                            String s = "4";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                        @Test
+                        void testTestFileRelativePath_5() {
+                            String s = "5";
+                            assertNotNull(s);
+                            assertTrue(s.length() >= 0);
+                            assertTrue(s.length() < 2);
+                        }
+                    }
+                """})
+    void testParameterizedTestWithMultipleSources(String originalSourceCode) throws RefactoringMinerTimedOutException {
+        String parameterizedTestCode = """
+                public class TestClass {
+                    @ParameterizedTest
+                    @EmptySource
+                    @ValueSource(strings={"1","2","3","4","5"})
+                    void testTestFileRelativePath(String s) {
+                        assertNotNull(s);
+                        assertTrue(s.length() >= 0);
+                        assertTrue(s.length() < 2);
+                    }
+                }
+                """;
+        UMLModel originalModel = createUmlModel(originalSourceCode);
+        UMLModel newModel = createUmlModel(parameterizedTestCode);
+        UMLModelDiff diff = originalModel.diff(newModel);
+        List<Refactoring> refactorings = diff.getRefactorings();
+        assertEquals(6, refactorings.size());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(0).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(1).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(2).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(3).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(4).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(5).getName());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "(TestEnum.class)"})
+    void testEnumSource(String sourceParameter) throws RefactoringMinerTimedOutException {
+        Set<String> dirs = Set.of("src/", "src/main/", "src/main/java", "src/main/java/test");
+        Map<String, String> files = Map.of(
+                "src/main/java/com/test/TestEnum.java", """
+                        package com.test;
+                        public enum TestEnum {
+                            TEST1, TEST2, TEST3, TEST4, TEST5;
+                            int number() {
+                                return Integer.parseInt(name().substring(name().length() - 1));
+                            }
+                        }
+                        """,
+                "src/test/java/com/test/TestClass.java", """
+                        package com.test;
+                        import org.junit.jupiter.params.ParameterizedTest;
+                        import org.junit.jupiter.params.provider.EnumSource;
+                        import static org.junit.jupiter.api.Assertions.assertTrue;
+                        public class TestClass {
+                            @ParameterizedTest
+                            @EnumSource%s
+                            void testEnum(TestEnum te) {
+                                assertTrue(te.number() >= 1 && te.number() <= 5);
+                            }
+                        }
+                """.formatted(sourceParameter));
+        HashMap<String, String> oldFiles = new HashMap<>(files);
+        oldFiles.replace("src/test/java/com/test/TestClass.java", """
+                        package com.test;
+                        import org.junit.jupiter.api.Test;
+                        import static org.junit.jupiter.api.Assertions.assertTrue;
+                        public class TestClass {
+                            @Test
+                            void testEnum_1() {
+                                assertTrue(TestEnum.TEST1.number() >= 1 && TestEnum.TEST1.number() <= 5);
+                            }
+                            @Test
+                            void testEnum_2() {
+                                assertTrue(TestEnum.TEST2.number() >= 1 && TestEnum.TEST2.number() <= 5);
+                            }
+                            @Test
+                            void testEnum_3() {
+                                assertTrue(TestEnum.TEST3.number() >= 1 && TestEnum.TEST3.number() <= 5);
+                            }
+                            @Test
+                            void testEnum_4() {
+                                assertTrue(TestEnum.TEST4.number() >= 3 && TestEnum.TEST4.number() <= 5);
+                            }
+                            @Test
+                            void testEnum_5() {
+                                assertTrue(TestEnum.TEST5.number() >= 3 && TestEnum.TEST5.number() <= 5);
+                            }
+                        }
+                """);
+        UMLModel newModel = new UMLModelASTReader(files, dirs, false).getUmlModel();
+        UMLModel originalModel = new UMLModelASTReader(oldFiles, dirs, false).getUmlModel();
+        UMLModelDiff diff = originalModel.diff(newModel);
+        List<Refactoring> refactorings = diff.getRefactorings();
+        assertEquals(5, refactorings.size());
+    }
+
+    @EnabledIf("isVictorParameterizedTestDisabled")
+    @Test
+    void testNullSrc() throws RefactoringMinerTimedOutException {
+        String originalSourceCode = """
+                public class TestClass {
+                    enum TestEnum {
+                         TEST1, TEST2, TEST3, TEST4, TEST5;
+                         int number() {
+                            if (name() == "TEST1") {
+                                throw new NullPointerException();
+                            }
+                            return Integer.parseInt(name().substring(name().length() - 1));
+                         }
+                    }
+                    @ParameterizedTest
+                    @EnumSource(value = TestEnum.class, names = {"TEST3", "TEST4", "TEST5"})
+                    void testEnum(TestEnum te) {
+                        assertDoesNotThrow(() -> te.number());
+                    }
+                    @Test
+                    void testNullEnum_TEST2() {
+                        assertDoesNotThrow(() -> TestEnum.TEST2.number());
+                    }
+                    @Test
+                    void testNullEnum_TEST1() {
+                        assertThrows(NullPointerException.class, () -> TestEnum.TEST1.number());
+                    }
+                    @Test
+                    void testNullEnum_null() {
+                        TestEnum te = null;
+                        assertThrows(NullPointerException.class, () -> te.number());
+                    }
+                }
+                """;
+        assertDoesNotThrow(() -> createUmlModel(originalSourceCode));
+        String parameterizedTestCode = """
+                    import org.junit.jupiter.params.ParameterizedTest;
+                    public class TestClass {
+                        enum TestEnum {
+                             TEST1, TEST2, TEST3, TEST4, TEST5;
+                             int number() {
+                                if (name() == "TEST1") {
+                                    throw new NullPointerException();
+                                }
+                                return Integer.parseInt(name().substring(name().length() - 1));
+                             }
+                        }
+                        @ParameterizedTest
+                        @NullSource
+                        @EnumSource(value = TestEnum.class, names = {"TEST1"})
+                        void testNullEnum(TestEnum te) {
+                            assertThrows(NullPointerException.class, () -> te.number());
+                        }
+                        @ParameterizedTest
+                        @EnumSource(value = TestEnum.class, names = {"TEST2", "TEST3", "TEST4", "TEST5"})
+                        void testEnum(TestEnum te) {
+                            assertDoesNotThrow(() -> te.number());
+                        }
+                    }""";
+        UMLModel originalModel = createUmlModel(originalSourceCode);
+        UMLModel newModel = createUmlModel(parameterizedTestCode);
+        UMLModelDiff diff = originalModel.diff(newModel);
+        List<Refactoring> refactorings = diff.getRefactorings();
+        assertEquals(5, refactorings.size());
+        assertEquals(RefactoringType.RENAME_METHOD.getDisplayName(), refactorings.get(0).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_VARIABLE.getDisplayName(), refactorings.get(1).getName());
+        assertEquals(RefactoringType.REMOVE_METHOD_ANNOTATION.getDisplayName(), refactorings.get(2).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(3).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(4).getName());
+    }
+
+    @EnabledIf("isVictorParameterizedTestDisabled")
+    @Test
+    void testNullAndEmptySource() throws RefactoringMinerTimedOutException {
+        String originalSourceCode = """
+                public class TestClass {
+                    @Test
+                    void testTestFileRelativePath_null() {
+                         try {
+                             new MyClass(null);
+                             fail();
+                         } catch (IllegalArgumentException e) {}
+                    }
+                    @Test
+                    void testTestFileRelativePath_empty() {
+                         try {
+                             new MyClass("");
+                             fail();
+                         } catch (IllegalArgumentException e) {}
+                    }
+                }
+                """;
+        String parameterizedTestCode = """
+                public class TestClass {
+                    @ParameterizedTest
+                    @NullAndEmptySource
+                    void testTestFileRelativePath(String s) {
+                         try {
+                             new MyClass(s);
+                             fail();
+                         } catch (IllegalArgumentException e) {}
+                    }
+                }
+                """;
+        UMLModel originalModel = createUmlModel(originalSourceCode);
+        UMLModel newModel = createUmlModel(parameterizedTestCode);
+        UMLModelDiff diff = originalModel.diff(newModel);
+        List<Refactoring> refactorings = diff.getRefactorings();
+        assertEquals(2, refactorings.size());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(0).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(1).getName());
+    }
+
+    @EnabledIf("isVictorParameterizedTestDisabled")
+    @Test
+    void testValueSource() throws RefactoringMinerTimedOutException {
+        String originalSourceCode = """
+                public class TestClass {
+                    @Test
+                    void testTestFileRelativePath_null() {
+                        assertThrows(InvalidParameterException.class, () -> {
+                            throw new InvalidParameterException();
+                        });
+                    }
+                    @Test
+                    void testTestFileRelativePath_empty() {
+                        assertThrows(IllegalArgumentException.class, () -> {
+                            throw new InvalidParameterException();
+                        });
+                    }
+                }
+                """;
+        String parameterizedTestCode = """
+                public class TestClass {
+                    @ParameterizedTest
+                    @ValueSource(classes = {InvalidParameterException.class, IllegalArgumentException.class})
+                    void testTestFileRelativePath(Class<? extends Exception> aClass) {
+                        assertThrows(aClass, () -> {
+                            throw new InvalidParameterException();
+                        });
+                    }
+                }
+                """;
+        UMLModel originalModel = createUmlModel(originalSourceCode);
+        UMLModel newModel = createUmlModel(parameterizedTestCode);
+        UMLModelDiff diff = originalModel.diff(newModel);
+        List<Refactoring> refactorings = diff.getRefactorings();
+        assertEquals(2, refactorings.size());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(0).getName());
+        assertEquals(RefactoringType.PARAMETERIZE_TEST.getDisplayName(), refactorings.get(1).getName());
     }
     @EnabledIf("isVictorParameterizedTestDisabled")
     @Nested

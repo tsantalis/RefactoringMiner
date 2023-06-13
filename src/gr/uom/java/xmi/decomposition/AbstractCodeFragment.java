49f5f17881d16f3e6f0f9d85ab0e2d378aa0069c
tsantalis <tsantalis@gmail.com>
2023-06-13 05:23:27 -0400

    Further improvement over previous commit
    Closure-148

--- src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
+++ src/gr/uom/java/xmi/decomposition/AbstractCodeFragment.java
@@ -160,9 +160,9 @@ public abstract class AbstractCodeFragment implements LocationInfoProvider {
 				locations.add(expression.getLocationInfo());
 			}
 		}
-		for(LeafExpression expression : getTernaryOperatorExpressions()) {
+		for(TernaryOperatorExpression expression : getTernaryOperatorExpressions()) {
 			if(expression.getString().equals(s)) {
-				matchingExpressions.add(expression);
+				matchingExpressions.add(expression.asLeafExpression());
 				locations.add(expression.getLocationInfo());
 			}
 		}

ac99fae9bb94320a61c11462972785551aa8d0e2
Pouryafard75 <pouryafard75@gmail.com>
2023-06-28 13:05:13 -0400

    Migrate to JSONAssert library to handle the diff tests

--- src-test/org/refactoringminer/astDiff/tests/Defects4JPerfectDiffTest.java
+++ src-test/org/refactoringminer/astDiff/tests/Defects4JPerfectDiffTest.java
@@ -1,18 +1,16 @@
 package org.refactoringminer.astDiff.tests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.joshka.junit.json.params.JsonFileSource;
 import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.refactoringminer.astDiff.actions.ASTDiff;
 import org.refactoringminer.astDiff.utils.CaseInfo;
 import org.refactoringminer.astDiff.utils.MappingExportModel;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,9 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.refactoringminer.astDiff.utils.UtilMethods.*;
 
@@ -33,8 +29,8 @@ import static org.refactoringminer.astDiff.utils.UtilMethods.*;
 public class Defects4JPerfectDiffTest {
     private static final String dir = getDefects4jMappingPath();
     @ParameterizedTest(name= "{index}: File: {2}, Repo: {0}, Commit: {1}")
-    @JsonFileSource(resources = "/astDiff/defects4j/cases-problematic.json")
-    public void testSubTreeMappings(@ConvertWith(CaseInfo.CaseInfoConverter.class) CaseInfo info) throws JsonProcessingException {
+    @JsonFileSource(resources = "/astDiff/defects4j/cases.json")
+    public void testSubTreeMappings(@ConvertWith(CaseInfo.CaseInfoConverter.class) CaseInfo info) throws JsonProcessingException, JSONException {
         File mappingsDirFile = new File(getFinalFolderPath(dir, info.getRepo(), info.getCommit()));
         String[] files = mappingsDirFile.list();
         List<String> expectedFilesList = new ArrayList<>(List.of(Objects.requireNonNull(files)));
@@ -51,11 +47,10 @@ public class Defects4JPerfectDiffTest {
             String expected = null;
             try {
                 expected = FileUtils.readFileToString(new File(finalFilePath), "utf-8");
-                assertEquals(expected.length(), calculated.length(), msg);
             } catch (IOException e) {
                 fail("File not found: " + finalFilePath + " for " + info.getRepo() + "/commit/" + info.getCommit() + " , srcFileName: " + astDiff.getSrcPath());
             }
-            assertEquals(calculated, expected, msg);
+            JSONAssert.assertEquals(msg,expected, calculated, false);
             expectedFilesList.remove(getFileNameFromSrcDiff(astDiff.getSrcPath()));
         }
         for (String expectedButNotGeneratedFile : expectedFilesList) {

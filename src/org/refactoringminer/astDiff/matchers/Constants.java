950e0855f021f744ee7680b65183d2e136e92f26
Pouryafard75 <pouryafard75@gmail.com>
2023-07-02 20:18:22 -0400

    Fix for all the problems related to static initializers

--- src/org/refactoringminer/astDiff/matchers/Constants.java
+++ src/org/refactoringminer/astDiff/matchers/Constants.java
@@ -72,4 +72,5 @@ public class Constants {
 	public static final String SIMPLE_TYPE = "SimpleType";
     public static final String EXPRESSION_METHOD_REFERENCE = "ExpressionMethodReference";
     public static final String PREFIX_EXPRESSION = "PrefixExpression";
+    public static final String INITIALIZER = "Initializer";
 }

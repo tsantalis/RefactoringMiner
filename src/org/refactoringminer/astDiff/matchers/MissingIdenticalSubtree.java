8827c49d001ac7a3c4b9a4d81502a302bbe53a71
Pouryafard75 <pouryafard75@gmail.com>
2023-07-10 22:14:12 -0400

    ASTDiff fix for ambiguous while finding the missing subtrees

--- src/org/refactoringminer/astDiff/matchers/MissingIdenticalSubtree.java
+++ src/org/refactoringminer/astDiff/matchers/MissingIdenticalSubtree.java
@@ -2,6 +2,8 @@ package org.refactoringminer.astDiff.matchers;
 
 import com.github.gumtreediff.matchers.*;
 import com.github.gumtreediff.matchers.heuristic.gt.DefaultPriorityTreeQueue;
+import com.github.gumtreediff.matchers.heuristic.gt.GreedySubtreeMatcher;
+import com.github.gumtreediff.matchers.heuristic.gt.MappingComparators;
 import com.github.gumtreediff.matchers.heuristic.gt.PriorityTreeQueue;
 import com.github.gumtreediff.tree.Tree;
 import org.refactoringminer.astDiff.utils.TreeUtilFunctions;
@@ -10,7 +12,7 @@ import java.util.*;
 import java.util.function.Function;
 
 /* Created by pourya on 2023-06-14 2:10 p.m. */
-public class MissingIdenticalSubtree implements TreeMatcher {
+public class MissingIdenticalSubtree extends GreedySubtreeMatcher implements TreeMatcher {
     private static final int DEFAULT_MIN_PRIORITY = 1;
     protected int minPriority = DEFAULT_MIN_PRIORITY;
 
@@ -59,11 +61,14 @@ public class MissingIdenticalSubtree implements TreeMatcher {
 
         filterMappings(multiMappings);
     }
-
+    @Override
     public void filterMappings(MultiMappingStore multiMappings) {
         List<Mapping> ambiguousList = new ArrayList<>();
         Set<Tree> ignored = new HashSet<>();
         for (var src : multiMappings.allMappedSrcs()) {
+            var isMappingUnique = false;
+            if (tinyTrees(src,multiMappings,minPriority))
+                continue;
             if (multiMappings.isSrcUnique(src)) {
                 var dst = multiMappings.getDsts(src).stream().findAny().get();
                 if (multiMappings.isDstUnique(dst)) {
@@ -77,6 +82,56 @@ public class MissingIdenticalSubtree implements TreeMatcher {
                     }
 
                 }
+                isMappingUnique = true;
+            }
+            if (!tinyTrees(src,multiMappings,minPriority) && !(ignored.contains(src) || isMappingUnique))
+            {
+
+                var adsts = multiMappings.getDsts(src);
+                var asrcs = multiMappings.getSrcs(multiMappings.getDsts(src).iterator().next());
+                for (Tree asrc : asrcs)
+                    for (Tree adst : adsts) {
+                        ambiguousList.add(new Mapping(asrc, adst));
+                    }
+                ignored.addAll(asrcs);
+            }
+            Set<Tree> srcIgnored = new HashSet<>();
+            Set<Tree> dstIgnored = new HashSet<>();
+            Collections.sort(ambiguousList, new MappingComparators.FullMappingComparator(mappings.getMonoMappingStore()));
+            // Select the best ambiguous mappings
+            retainBestMapping(ambiguousList, srcIgnored, dstIgnored);
+        }
+    }
+
+    private static boolean tinyTrees(Tree src, MultiMappingStore multiMappings, int minP) {
+        if (src.getMetrics().height <= minP){
+            if (src.getType().name.equals(Constants.METHOD_INVOCATION_RECEIVER))
+                return true;
+            if (src.getType().name.equals(Constants.METHOD_INVOCATION_ARGUMENTS))
+                return true;
+            if (src.getType().name.equals(Constants.SIMPLE_TYPE ))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void retainBestMapping(List<Mapping> mappingList, Set<Tree> srcIgnored, Set<Tree> dstIgnored) {
+        List<Mapping> verifiedList = new ArrayList<>();
+        for (Mapping mapping : mappingList) {
+            if (TreeUtilFunctions.areBothFromThisType(mapping, Constants.SIMPLE_NAME) || TreeUtilFunctions.areBothFromThisType(mapping, Constants.QUALIFIED_NAME)) {
+                    verifiedList.add(mapping);
+            }
+            else verifiedList.add(mapping);
+        }
+        while (verifiedList.size() > 0) {
+            var mapping = verifiedList.remove(0);
+            if (!(srcIgnored.contains(mapping.first) || dstIgnored.contains(mapping.second))) {
+                mappings.addMappingRecursively(mapping.first, mapping.second);
+                srcIgnored.add(mapping.first);
+                srcIgnored.addAll(mapping.first.getDescendants());
+                dstIgnored.add(mapping.second);
+                dstIgnored.addAll(mapping.second.getDescendants());
             }
         }
     }

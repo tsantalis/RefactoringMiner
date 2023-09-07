c126f2ff6e2cc3e16b88318c640f9297be3c840a
Pouryafard75 <pouryafard75@gmail.com>
2023-09-07 16:38:45 -0400

    ASTDiff: Add a comment for cases which generate more than 2 ASTDiff files but already validated

--- src-test/org/refactoringminer/astDiff/tests/RefactoringOraclePerfectDiffTest.java
+++ src-test/org/refactoringminer/astDiff/tests/RefactoringOraclePerfectDiffTest.java
@@ -57,3 +57,13 @@ public class RefactoringOraclePerfectDiffTest {
         }
     }
 }
+
+/*
+List of already generated diffs but removed due to having more than 2 AST files:
+    https://github.com/mongodb/mongo-java-driver/commit/8c5a20d786e66ee4c4b0d743f0f80bf681c419be
+    https://github.com/ratpack/ratpack/commit/2581441eda268c45306423dd4c515514d98a14a0
+    https://github.com/gradle/gradle/commit/ba1da95200d080aef6251f13ced0ca67dff282be
+    https://github.com/brettwooldridge/HikariCP/commit/1571049ec04b1e7e6f082ed5ec071584e7200c12
+    https://github.com/spring-attic/spring-roo/commit/0bb4cca1105fc6eb86e7c4b75bfff3dbbd55f0c8
+    https://github.com/hazelcast/hazelcast/commit/f1e26fa73074a89680a2e1756d85eb80ad87c3bf
+*/

ced85c695f305adad241e97eca0417daabf5b77e
tsantalis <tsantalis@gmail.com>
2023-12-06 08:00:25 -0500

    Update total number of true positives

--- src/test/java/org/refactoringminer/test/TestAllRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestAllRefactorings.java
@@ -15,6 +15,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, REPOS, Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectationsWithGitHubAPI(11885, 23, 255);
+		test.assertExpectationsWithGitHubAPI(11949, 23, 255);
 	}
 }

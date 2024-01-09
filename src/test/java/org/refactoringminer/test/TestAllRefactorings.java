213941bf5aeab98d3e92b17f41c913b9201dd83f
tsantalis <tsantalis@gmail.com>
2024-01-08 22:24:57 -0500

    More fine-grained type change analysis
    https://github.com/pmd/pmd/commit/33bfd00303daa93eebc91a1d0b31567192becdcc#diff-d098af0a8c4a529251028361cbe0394315398ad3341beff9d69cf4e050457bb0R45

--- src/test/java/org/refactoringminer/test/TestAllRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestAllRefactorings.java
@@ -15,6 +15,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, REPOS, Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectationsWithGitHubAPI(12027, 20, 243);
+		test.assertExpectationsWithGitHubAPI(12029, 20, 243);
 	}
 }

1f7fcf3783d882a0ddde5ae869227a1e6b6f20cd
Pouryafard75 <pouryafard75@gmail.com>
2024-05-16 22:24:47 -0400

    ASTDiff: Alternative implementation for MovedASTDiff generator

---
+++ src/main/java/org/refactoringminer/astDiff/AllSubTreesMovedASTDiffGenerator.java
@@ -0,0 +1,42 @@
+package org.refactoringminer.astDiff;
+
+import com.github.gumtreediff.actions.model.Action;
+import com.github.gumtreediff.matchers.Mapping;
+import com.github.gumtreediff.tree.Tree;
+import com.github.gumtreediff.utils.Pair;
+import gr.uom.java.xmi.diff.UMLModelDiff;
+import org.refactoringminer.astDiff.actions.ASTDiff;
+import org.refactoringminer.astDiff.actions.ExtendedOnlyRootsClassifier;
+import org.refactoringminer.astDiff.actions.ProjectASTDiff;
+import org.refactoringminer.astDiff.actions.model.MoveOut;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/* Created by pourya on 2024-05-16*/
+public class AllSubTreesMovedASTDiffGenerator extends MovedASTDiffGenerator {
+
+    public AllSubTreesMovedASTDiffGenerator(UMLModelDiff modelDiff, ProjectASTDiff projectASTDiff) {
+        super(modelDiff, projectASTDiff);
+    }
+
+    @Override
+    public void populateFilePairMappings() {
+        for(ASTDiff diff : projectASTDiff.getDiffSet()) {
+            ExtendedOnlyRootsClassifier classifier = (ExtendedOnlyRootsClassifier) diff.createRootNodesClassifier(); // TODO: Switch to AllNodesClassifier
+            for (Map.Entry<Tree, Action> treeActionEntry : classifier.getSrcMoveOutTreeMap().entrySet()) {
+                MoveOut moveOut = (MoveOut) treeActionEntry.getValue();
+                String srcPath = diff.getSrcPath();
+                String dstPath = moveOut.getDstFile();
+                Pair<String, String> pair = new Pair<>(srcPath, dstPath);
+                if (!filePairMappings.containsKey(pair)) {
+                    filePairMappings.put(pair, new ArrayList<>());
+                }
+                List<Mapping> target = filePairMappings.get(pair);
+                target.add(new Mapping(moveOut.getNode(), moveOut.getParent()));
+            }
+        }
+    }
+}

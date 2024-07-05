7ac4b344940c948650858680134681d925dca273
Pouryafard75 <pouryafard75@gmail.com>
2024-07-05 04:35:04 -0400

    ASTDiff GUI: Remove logs

--- src/main/resources/web/dist/single.js
+++ src/main/resources/web/dist/single.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
-    // Function to resize all iframes based on their content
-    console.log("single.js loaded");
 
     function resizeAllIframes() {
-        console.log("resizeAllIframes");
         $('iframe').each(function() {
             var iframe = this;
             iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
         });
     }
 
-    // Call resizeAllIframes when each iframe is loaded and when the window is resized
     $('iframe').on('load', function() {
         resizeAllIframes();
     });

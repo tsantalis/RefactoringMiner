85b96ee0bbbc8f61a510d055ec7388d049f6d0d9
Pouryafard75 <pouryafard75@gmail.com>
2024-07-06 22:11:17 -0400

    ASTDiff GUI: Improve the scrolling

--- src/main/resources/web/dist/single.js
+++ src/main/resources/web/dist/single.js
@@ -5,15 +5,29 @@ $(document).ready(function() {
         var id = $(this).attr('id');
         $(this).attr('href', '#' + id);
 
-        // Add click event listener to scroll the right panel
+        // Add click event listener to scroll the accordion panel
         $(this).click(function(event) {
             event.preventDefault(); // Prevent the default anchor behavior
             var targetId = 'heading-' + id.split('_')[2];
-            console.log(targetId);
-            // Scroll the monaco panel to the element with the corresponding heading ID
-            $('#accordion').animate({
-                scrollTop: $('#' + targetId).offset().top
-            }, 500); // Adjust the scroll duration as needed
+            var $target = $('#' + targetId);
+            var targetOffset = $('#' + targetId).offset().top;
+            var accordionOffset = $('#accordion').offset().top;
+            var scrollTo = targetOffset - accordionOffset + $('#accordion').scrollTop();
+            // console.log($target)
+
+            // Check if the target is already in view
+            if (Math.abs($('#accordion').scrollTop() - scrollTo) < 1) {
+                // If it's already in view, shake it
+                $target.effect('shake', { direction: 'up', distance: 10, times: 3 }, 500);
+            } else {
+                // Scroll the accordion panel to the element with the corresponding heading ID
+                $('#accordion').animate({
+                    scrollTop: scrollTo
+                }, 500, function() {
+                    // Optionally shake after scrolling
+                    // $target.effect('shake', { direction: 'up', distance: 10, times: 3 }, 500);
+                }); // Adjust the scroll duration as needed
+            }
         });
     });
 });

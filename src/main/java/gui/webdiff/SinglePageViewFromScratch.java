72cff5835ce8765d851d973fab1464550c06c29c
Pouryafard75 <pouryafard75@gmail.com>
2024-07-22 20:46:58 -0400

    ASTDiff GUI: More refactorings

--- src/main/java/gui/webdiff/SinglePageViewFromScratch.java
+++ src/main/java/gui/webdiff/SinglePageViewFromScratch.java
@@ -5,15 +5,26 @@ import org.rendersnake.Renderable;
 
 import java.io.IOException;
 
-/* Created by pourya on 2024-07-22*/
-public class SinglePageViewFromScratch implements Renderable {
+import static org.rendersnake.HtmlAttributesFactory.*;
 
+/* Created by pourya on 2024-07-22*/
+public class SinglePageViewFromScratch extends AbstractSinglePageView implements Renderable {
     public SinglePageViewFromScratch(DirComparator comparator) {
-        //TODO:
+        super(comparator);
     }
 
     @Override
-    public void renderOn(HtmlCanvas html) throws IOException {
+    protected HtmlCanvas addJSMacros(HtmlCanvas html) throws IOException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    protected void makeEachDiff(HtmlCanvas html, int i) throws IOException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    protected void makeHead(HtmlCanvas html) throws IOException {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 }

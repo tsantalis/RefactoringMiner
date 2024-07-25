c2d66b2fb6e0aed4b5702b1023df9548b307572e
tsantalis <tsantalis@gmail.com>
2024-07-25 06:36:44 -0400

    Statement mapping improvement in commit
    https://github.com/eclipse-jgit/jgit/commit/8ac65d33e

--- src/main/java/gr/uom/java/xmi/decomposition/ScopedCompositeMappingComparatorForExtract.java
+++ src/main/java/gr/uom/java/xmi/decomposition/ScopedCompositeMappingComparatorForExtract.java
@@ -13,11 +13,13 @@ public class ScopedCompositeMappingComparatorForExtract implements Comparator<Co
 	public int compare(CompositeStatementObjectMapping o1, CompositeStatementObjectMapping o2) {
 		if(parentMapping != null) {
 			if(parentMapping.getFragment1().getLocationInfo().subsumes(o1.getFragment1().getLocationInfo()) &&
+					!parentMapping.getFragment1().equals(o1.getFragment1()) &&
 					!parentMapping.getFragment1().getLocationInfo().subsumes(o2.getFragment1().getLocationInfo())) {
 				return -1;
 			}
 			else if(!parentMapping.getFragment1().getLocationInfo().subsumes(o1.getFragment1().getLocationInfo()) &&
-					parentMapping.getFragment1().getLocationInfo().subsumes(o2.getFragment1().getLocationInfo())) {
+					parentMapping.getFragment1().getLocationInfo().subsumes(o2.getFragment1().getLocationInfo()) &&
+					!parentMapping.getFragment1().equals(o2.getFragment1())) {
 				return 1;
 			}
 		}

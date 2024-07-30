d4740570ed3d1dc2bef2118e80b3787a6401e970
Pouryafard75 <pouryafard75@gmail.com>
2024-07-30 16:31:19 -0400

    ASTDiff GUI: Make the SinglePageView work without any duplication problems

--- src/main/java/gui/webdiff/viewers/spv/AbstractSinglePageView.java
+++ src/main/java/gui/webdiff/viewers/spv/AbstractSinglePageView.java
@@ -8,6 +8,8 @@ import org.rendersnake.HtmlCanvas;
 import org.rendersnake.Renderable;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.rendersnake.HtmlAttributesFactory.*;
 
@@ -15,13 +17,10 @@ import static org.rendersnake.HtmlAttributesFactory.*;
 public abstract class AbstractSinglePageView extends DirectoryDiffView implements Renderable {
     protected final String JQ_UI_CSS = "https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css";
     protected final String JQ_UI_JS = "https://code.jquery.com/ui/1.12.1/jquery-ui.js";
+    protected final List<MonacoCore> cores = new ArrayList<>();
 
     public AbstractSinglePageView(DirComparator comparator) {
         super(comparator);
-    }
-
-    public AbstractSinglePageView(DirComparator comparator, boolean external) {
-        super(comparator, external);
     }
 
     @Override
@@ -47,6 +46,7 @@ public abstract class AbstractSinglePageView extends DirectoryDiffView implement
             // Generate panels for /monaco-0 to /monaco-n
             for (int i = 0; i < n; i++) {
                 MonacoCore core = new MonacoCore(comparator.getASTDiff(i), i, comparator.isMoveDiff(i));
+                cores.add(core);
                 core.setShowFilenames(false);
                 html.div(class_("card"))
                         .div(class_("card-header").id("heading-" + i).style("padding-right: 0;"))
@@ -64,9 +64,10 @@ public abstract class AbstractSinglePageView extends DirectoryDiffView implement
                         .a(href("monaco-page/" + i).class_("btn btn-primary btn sm")).content("Details")
                         ._div()
                         ._div()
+                        ._div()
                         .div(id("collapse-" + i).class_("collapse show").add("aria-labelledby", "heading-" + i))
                         .div(class_("card-body").style("padding: 0;"));
-                makeEachDiff(html, i);
+                makeEachDiff(html, i, core);
                 html
                         ._div()
                         ._div()
@@ -94,7 +95,7 @@ public abstract class AbstractSinglePageView extends DirectoryDiffView implement
 
     protected abstract HtmlCanvas addJSMacros(HtmlCanvas html) throws IOException;
 
-    protected abstract void makeEachDiff(HtmlCanvas html, int i) throws IOException;
+    protected abstract void makeEachDiff(HtmlCanvas html, int i, MonacoCore core) throws IOException;
 
     protected abstract void makeHead(HtmlCanvas html) throws IOException;
 }

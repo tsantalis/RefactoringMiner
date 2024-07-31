d465813e41ca1d4a42324cb78f264a26889e1da6
tsantalis <tsantalis@gmail.com>
2024-07-31 16:24:34 -0400

    Include enum constants when computing move/rename class candidates

--- src/test/java/org/refactoringminer/test/TestAllRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestAllRefactorings.java
@@ -15,6 +15,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, REPOS, Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectationsWithGitHubAPI(12264, 20, 236);
+		test.assertExpectationsWithGitHubAPI(12265, 20, 236);
 	}
 }

e0522578a81489a577aa2794535f7ce703cd8ca4
tsantalis <tsantalis@gmail.com>
2024-10-02 11:49:32 -0400

    Process consecutive line comments in groups, as if they were one comment

---
+++ src/main/java/gr/uom/java/xmi/UMLCommentGroup.java
@@ -0,0 +1,38 @@
+package gr.uom.java.xmi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class UMLCommentGroup {
+	private List<UMLComment> group;
+
+	public UMLCommentGroup() {
+		this.group = new ArrayList<UMLComment>();
+	}
+
+	public UMLCommentGroup(List<UMLComment> group) {
+		this.group = group;
+	}
+
+	public void addComment(UMLComment c) {
+		group.add(c);
+	}
+
+	public List<UMLComment> getGroup() {
+		return group;
+	}
+
+	public boolean sameText(UMLCommentGroup other) {
+		if(this.group.size() == other.group.size()) {
+			int matches = 0;
+			for(int i=0; i<this.group.size(); i++) {
+				if(this.group.get(i).getText().equals(other.group.get(i).getText())) {
+					matches++;
+				}
+			}
+			return matches == this.group.size();
+		}
+		return false;
+	}
+}

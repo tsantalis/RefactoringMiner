61f7d0f3155746ee44b138295cfc7e986cdf725b
tsantalis <tsantalis@gmail.com>
2024-11-21 13:42:43 -0500

    Reorder conditions being checked

--- src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
+++ src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
@@ -283,7 +283,7 @@ public class UMLOperationBodyMapper implements Comparable<UMLOperationBodyMapper
 			if(classDiff != null) {
 				List<String> list1 = body1.stringRepresentation();
 				for(UMLOperation addedOperation : classDiff.getAddedOperations()) {
-					boolean skip = operation1.equalParameters(addedOperation) && classDiff.getAddedOperations().size() == classDiff.getRemovedOperations().size();
+					boolean skip = classDiff.getAddedOperations().size() == classDiff.getRemovedOperations().size() && operation1.equalParameters(addedOperation);
 					if(addedOperation.getBody() != null && !skip) {
 						List<String> list2 = new ArrayList<>(addedOperation.getBody().stringRepresentation());
 						if(list2.size() > 3) {

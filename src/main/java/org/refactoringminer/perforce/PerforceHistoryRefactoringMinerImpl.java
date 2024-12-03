469c3e7045dccec0fd3bba7e8b7fe1f8e076baed
tsantalis <tsantalis@gmail.com>
2024-12-03 06:18:05 -0500

    Implement Perforce refactoring detection APIs + handle thrown exceptions

--- src/main/java/org/refactoringminer/perforce/PerforceHistoryRefactoringMinerImpl.java
+++ src/main/java/org/refactoringminer/perforce/PerforceHistoryRefactoringMinerImpl.java
@@ -1,10 +1,22 @@
 package org.refactoringminer.perforce;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.refactoringminer.api.PerforceHistoryRefactoringMiner;
+import org.refactoringminer.api.Refactoring;
 import org.refactoringminer.api.RefactoringHandler;
+import org.refactoringminer.api.RefactoringMinerTimedOutException;
+import org.refactoringminer.api.RefactoringType;
 import org.refactoringminer.astDiff.matchers.ProjectASTDiffer;
 import org.refactoringminer.astDiff.models.ProjectASTDiff;
 import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
@@ -19,6 +31,7 @@ import com.perforce.p4java.server.IOptionsServer;
 import com.perforce.p4java.server.ServerFactory;
 
 import gr.uom.java.xmi.UMLModel;
+import gr.uom.java.xmi.diff.MoveSourceFolderRefactoring;
 import gr.uom.java.xmi.diff.UMLModelDiff;
 
 /**
@@ -27,7 +40,32 @@ import gr.uom.java.xmi.diff.UMLModelDiff;
  */
 public class PerforceHistoryRefactoringMinerImpl implements PerforceHistoryRefactoringMiner {
 	private final static Logger logger = LoggerFactory.getLogger(PerforceHistoryRefactoringMinerImpl.class);
+	private Set<RefactoringType> refactoringTypesToConsider = null;
 	private IOptionsServer perforceServer;
+
+	public PerforceHistoryRefactoringMinerImpl() {
+		this.setRefactoringTypesToConsider(RefactoringType.ALL);
+	}
+
+	public void setRefactoringTypesToConsider(RefactoringType ... types) {
+		this.refactoringTypesToConsider = new HashSet<RefactoringType>();
+		for (RefactoringType type : types) {
+			this.refactoringTypesToConsider.add(type);
+		}
+	}
+
+	protected List<Refactoring> filter(List<Refactoring> refactoringsAtRevision) {
+		if (this.refactoringTypesToConsider == null) {
+			return refactoringsAtRevision;
+		}
+		List<Refactoring> filteredList = new ArrayList<Refactoring>();
+		for (Refactoring ref : refactoringsAtRevision) {
+			if (this.refactoringTypesToConsider.contains(ref.getRefactoringType())) {
+				filteredList.add(ref);
+			}
+		}
+		return filteredList;
+	}
 
 	@Override
 	public IOptionsServer connectToPerforceServer(String serverUrl, String userName, String password) throws Exception {
@@ -47,27 +85,37 @@ public class PerforceHistoryRefactoringMinerImpl implements PerforceHistoryRefac
 	}
 
 	@Override
-	public ProjectASTDiff diffAtChangeList(String serverUrl, String userName, String password, int changeListNumber)
-			throws Exception {
-		// Connect to Perforce server.
-        this.perforceServer = connectToPerforceServer(serverUrl, userName, password);
-        ASTDiffInput astDiffInput = populateContentsForPerforceCl(changeListNumber);
-        UMLModel parentUMLModel = GitHistoryRefactoringMinerImpl.createModelForASTDiff(
-                astDiffInput.getFileContentsBefore(),
-                astDiffInput.getDirectoriesBefore());
-        UMLModel currentUMLModel = GitHistoryRefactoringMinerImpl.createModelForASTDiff(
-                astDiffInput.getFileContentsAfter(),
-                astDiffInput.getDirectoriesAfter());
-        UMLModelDiff modelDiff = parentUMLModel.diff(currentUMLModel);
-        ProjectASTDiffer differ = new ProjectASTDiffer(modelDiff, astDiffInput.getFileContentsBefore(), astDiffInput.getFileContentsAfter());
-        return differ.getProjectASTDiff();
+	public ProjectASTDiff diffAtChangeList(String serverUrl, String userName, String password, int changeListNumber) {
+		try {
+			// Connect to Perforce server.
+	        this.perforceServer = connectToPerforceServer(serverUrl, userName, password);
+	        ASTDiffInput astDiffInput = populateContentsForPerforceCl(changeListNumber);
+	        GitHistoryRefactoringMinerImpl.processIdenticalFiles(
+	        		astDiffInput.getFileContentsBefore(), 
+	        		astDiffInput.getFileContentsAfter(), 
+	        		Collections.emptyMap(), true);
+	        UMLModel parentUMLModel = GitHistoryRefactoringMinerImpl.createModelForASTDiff(
+	                astDiffInput.getFileContentsBefore(),
+	                astDiffInput.getDirectoriesBefore());
+	        UMLModel currentUMLModel = GitHistoryRefactoringMinerImpl.createModelForASTDiff(
+	                astDiffInput.getFileContentsAfter(),
+	                astDiffInput.getDirectoriesAfter());
+	        UMLModelDiff modelDiff = parentUMLModel.diff(currentUMLModel);
+	        ProjectASTDiffer differ = new ProjectASTDiffer(modelDiff, astDiffInput.getFileContentsBefore(), astDiffInput.getFileContentsAfter());
+	        return differ.getProjectASTDiff();
+		} catch (RefactoringMinerTimedOutException e) {
+			logger.warn(String.format("Ignored changeList %s due to timeout", changeListNumber), e);
+		} catch (Exception e) {
+			logger.warn(String.format("Ignored changeList %s due to error", changeListNumber), e);
+		}
+		return null;
     }
 
     private ASTDiffInput populateContentsForPerforceCl(int clNumber) throws Exception {
         // Create changelist object for changelist
         IChangelist changelist = perforceServer.getChangelist(clNumber);
         if (changelist == null) {
-        	logger.warn("Changelist %d not found".formatted(clNumber));
+        	logger.warn("ChangeList %d not found".formatted(clNumber));
             throw new Exception("null returned when getting the changelist %d".formatted(clNumber));
         }
         ASTDiffInput astDiffInput = new ASTDiffInput();
@@ -161,15 +209,52 @@ public class PerforceHistoryRefactoringMinerImpl implements PerforceHistoryRefac
 
 	@Override
 	public void detectAtChangeList(String serverUrl, String userName, String password, int changeListNumber,
-			RefactoringHandler handler) throws Exception {
-		// TODO Auto-generated method stub
-		
+			RefactoringHandler handler) {
+		try {
+			// Connect to Perforce server.
+	        this.perforceServer = connectToPerforceServer(serverUrl, userName, password);
+	        ASTDiffInput astDiffInput = populateContentsForPerforceCl(changeListNumber);
+	        List<MoveSourceFolderRefactoring> moveSourceFolderRefactorings = GitHistoryRefactoringMinerImpl.processIdenticalFiles(
+	        		astDiffInput.getFileContentsBefore(), 
+	        		astDiffInput.getFileContentsAfter(), 
+	        		Collections.emptyMap(), false);
+	        UMLModel parentUMLModel = GitHistoryRefactoringMinerImpl.createModel(
+	                astDiffInput.getFileContentsBefore(),
+	                astDiffInput.getDirectoriesBefore());
+	        UMLModel currentUMLModel = GitHistoryRefactoringMinerImpl.createModel(
+	                astDiffInput.getFileContentsAfter(),
+	                astDiffInput.getDirectoriesAfter());
+	        UMLModelDiff modelDiff = parentUMLModel.diff(currentUMLModel);
+	        List<Refactoring> refactoringsAtRevision = modelDiff.getRefactorings();
+			refactoringsAtRevision.addAll(moveSourceFolderRefactorings);
+			refactoringsAtRevision = filter(refactoringsAtRevision);
+			handler.handle(String.valueOf(changeListNumber), refactoringsAtRevision);
+			handler.handleModelDiff(String.valueOf(changeListNumber), refactoringsAtRevision, modelDiff);
+		} catch (RefactoringMinerTimedOutException e) {
+			logger.warn(String.format("Ignored changeList %s due to timeout", changeListNumber), e);
+		} catch (Exception e) {
+			logger.warn(String.format("Ignored changeList %s due to error", changeListNumber), e);
+			handler.handleException(String.valueOf(changeListNumber), e);
+		}
 	}
 
 	@Override
 	public void detectAtChangeList(String serverUrl, String userName, String password, int changeListNumber,
-			RefactoringHandler handler, int timeout) throws Exception {
-		// TODO Auto-generated method stub
-		
+			RefactoringHandler handler, int timeout) {
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		Future<?> f = null;
+		try {
+			Runnable r = () -> detectAtChangeList(serverUrl, userName, password, changeListNumber, handler);
+			f = service.submit(r);
+			f.get(timeout, TimeUnit.SECONDS);
+		} catch (TimeoutException e) {
+			f.cancel(true);
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			service.shutdown();
+		}
 	}
 }

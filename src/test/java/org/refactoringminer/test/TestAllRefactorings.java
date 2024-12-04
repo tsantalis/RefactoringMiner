90da06f9af1ebaf7676f24f39f92faf93f873079
tsantalis <tsantalis@gmail.com>
2024-12-04 12:57:05 -0500

    Enable the detection of Move and Rename Attribute based on references
    where a getter call is replaced with a variable.
    Move And Rename Attribute protected _unionClass : JClass renamed to private _unionBaseClass : JClass and moved from class com.linkedin.pegasus.generator.JavaCodeGeneratorBase to class com.linkedin.pegasus.generator.JavaDataTemplateGenerator
    
    https://github.com/linkedin/rest.li/commit/bd0d3bf75d31a8b5db34b8b66dfb28e5e1f492de

--- src/test/java/org/refactoringminer/test/TestAllRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestAllRefactorings.java
@@ -17,6 +17,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, REPOS, Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectationsWithGitHubAPI(12299, 20, 232);
+		test.assertExpectationsWithGitHubAPI(12301, 20, 232);
 	}
 }

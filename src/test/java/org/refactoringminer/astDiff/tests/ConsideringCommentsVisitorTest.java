da702e5422f7a9602ae9a24011646f3bc0b37fc4
Pouryafard75 <pouryafard75@gmail.com>
2024-12-09 06:47:02 -0500

    ASTDiff: RefactoringsOracle tests consider comments

---
+++ src/test/java/org/refactoringminer/astDiff/tests/ConsideringCommentsVisitorTest.java
@@ -0,0 +1,8 @@
+package org.refactoringminer.astDiff.tests;
+
+/* Created by pourya on 2024-12-09*/
+public class ConsideringCommentsVisitorTest {
+    ConsideringCommentsVisitorTest(){
+        System.setProperty("rm.jdt.comments", "true");
+    }
+}

2013f707c75bf877c7e53c77afe1946643419c54
tsantalis <tsantalis@gmail.com>
2024-12-09 18:41:49 -0500

    Removed call to getCommentListDiff(). Should be called only in AST diff

--- src/main/java/gr/uom/java/xmi/diff/UMLAbstractClassDiff.java
+++ src/main/java/gr/uom/java/xmi/diff/UMLAbstractClassDiff.java
@@ -97,7 +97,6 @@ public abstract class UMLAbstractClassDiff {
 		this.interfaceListDiff = new UMLTypeListDiff(originalClass.getImplementedInterfaces(), nextClass.getImplementedInterfaces());
 		this.permittedTypeListDiff = new UMLTypeListDiff(originalClass.getPermittedTypes(), nextClass.getPermittedTypes());
 		processAnnotations();
-		getCommentListDiff();
 	}
 
 	public List<UMLOperation> getAddedOperations() {

a7eeb0921977c6faa57fa1a86f5906de322f0e13
tsantalis <tsantalis@gmail.com>
2025-01-09 14:26:17 -0500

    Enable reporting of Lambda parameter renames
    https://github.com/spring-projects/spring-boot/commit/558d811b0a92c3bf24bbf688c7bd579137e593d0
    Rename Variable	zipEntry : null to jarEntry : null in method private extractLibraries(fileResolver FileResolver, jarStructure JarStructure, options Map<Option,String>) : void from class org.springframework.boot.jarmode.tools.ExtractCommand
    Rename Variable	zipEntry : null to jarEntry : null in method private extractArchive(fileResolver FileResolver, entryNameTransformer EntryNameTransformer) : void from class org.springframework.boot.jarmode.tools.ExtractCommand
    Rename Variable	zipEntry : null to jarEntry : null in method private createApplication(jarStructure JarStructure, fileResolver FileResolver, options Map<Option,String>) : void from class org.springframework.boot.jarmode.tools.ExtractCommand

--- src/main/java/gr/uom/java/xmi/decomposition/AbstractStatement.java
+++ src/main/java/gr/uom/java/xmi/decomposition/AbstractStatement.java
@@ -2,6 +2,8 @@ package gr.uom.java.xmi.decomposition;
 
 import java.util.List;
 
+import gr.uom.java.xmi.VariableDeclarationContainer;
+
 public abstract class AbstractStatement extends AbstractCodeFragment {
 	private CompositeStatementObject parent;
 	protected String actualSignature;
@@ -29,6 +31,18 @@ public abstract class AbstractStatement extends AbstractCodeFragment {
     	}
     	else if(parent != null) {
     		return parent.searchVariableDeclaration(variableName);
+    	}
+    	else if(this instanceof CompositeStatementObject) {
+    		CompositeStatementObject comp = (CompositeStatementObject)this;
+    		if(comp.getOwner().isPresent()) {
+    			VariableDeclarationContainer container = comp.getOwner().get();
+    			if(container instanceof LambdaExpressionObject) {
+	    			VariableDeclaration declaration = container.getVariableDeclaration(variableName);
+	    			if(declaration != null) {
+	    				return declaration;
+	    			}
+    			}
+    		}
     	}
     	return null;
     }

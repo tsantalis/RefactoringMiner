96f9d82591f5bad1d6f4872176a14d1c26194944
maorethians <apaladian@gmail.com>
2025-02-03 11:24:09 -0500

    feat: add graph processor changes

--- src/main/java/narrator/graph/GraphProcessor.java
+++ src/main/java/narrator/graph/GraphProcessor.java
@@ -1,6 +1,7 @@
 package narrator.graph;
 
 import com.github.gumtreediff.tree.Tree;
+import com.github.gumtreediff.tree.TreeContext;
 import gr.uom.java.xmi.*;
 import gr.uom.java.xmi.decomposition.AbstractCall;
 import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
@@ -23,26 +24,34 @@ public class GraphProcessor {
     private final UMLModel model;
     private final APTED editDistance = new APTED(new StringUnitCostModel());
     private final float distanceThreshold = 0;
+    private Map<String, String> dstContents;
+    Map<String, TreeContext> childContexts;
 
-    public GraphProcessor(UMLModelDiff modelDiff) {
+    public GraphProcessor(UMLModelDiff modelDiff, Map<String, String> dstContents, Map<String, TreeContext> childContexts) {
         graph = GraphTypeBuilder.<Node, Edge>directed().allowingMultipleEdges(true).allowingSelfLoops(true).edgeClass(Edge.class).weighted(true).buildGraph();
         nodeMap = new HashMap<>();
         this.modelDiff = modelDiff;
         this.model = modelDiff.getChildModel();
+        this.dstContents = dstContents;
+        this.childContexts = childContexts;
     }
 
-    public void addHunkNode(String fileContent, String path, Tree tree) {
-        Node node = new Node(fileContent, path, tree, false);
+    private Node addHunkNode(String path, Tree tree, NodeType nodeType) {
+        String fileContent = dstContents.get(path);
+
+        Node node = new Node(fileContent, path, tree, nodeType);
 
         if (!nodeMap.containsKey(node.getId())) {
             graph.addVertex(node);
             nodeMap.put(node.getId(), node);
+        } else {
+            node = nodeMap.get(node.getId());
         }
 
         Node lastNode = node;
         List<Tree> contexts = Context.get(tree);
         for (Tree context : contexts) {
-            Node contextNode = new Node(fileContent, path, context, true);
+            Node contextNode = new Node(fileContent, path, context, NodeType.CONTEXT);
             if (!nodeMap.containsKey(contextNode.getId())) {
                 graph.addVertex(contextNode);
                 nodeMap.put(contextNode.getId(), contextNode);
@@ -56,6 +65,11 @@ public class GraphProcessor {
             lastNode = contextNode;
         }
 
+        return node;
+    }
+
+    public Node addHunkNode(String path, Tree tree) {
+        return addHunkNode(path, tree, NodeType.BASE);
     }
 
     private void addEdge(Node node1, Node node2, EdgeType edgeType, float weight) {
@@ -72,15 +86,16 @@ public class GraphProcessor {
 
     public void process() {
         processDefUse();
+        processClassInstanceCreations();
         processOutOfClasses();
         processSimilarity();
         processSuccession();
-        processClassInstanceCreations();
         processContextMethodInvocations();
     }
 
     private void processDefUse() {
-        for (Node node : graph.vertexSet()) {
+        List<Node> nodes = new ArrayList<>(graph.vertexSet());
+        for (Node node : nodes) {
             if (node.isContext()) {
                 continue;
             }
@@ -113,6 +128,7 @@ public class GraphProcessor {
                 continue;
             }
 
+            // prevent having one edge for method and some other for parameters
             for (Tree methodDeclaration : methodDeclarations) {
                 List<Tree> methodDescendants = methodDeclaration.getDescendants();
                 for (Tree methodDescendant : methodDescendants) {
@@ -144,7 +160,7 @@ public class GraphProcessor {
             }
 
             for (Tree parameterDeclaration : parameterDeclarations) {
-                addVariableDeclarationUseEdges(umlClass, node, parameterDeclaration);
+                addVariableDeclarationEdges(umlClass, node, parameterDeclaration);
             }
 
             for (Tree variableDeclaration : variableDeclarations) {
@@ -154,7 +170,51 @@ public class GraphProcessor {
                     continue;
                 }
 
-                addVariableDeclarationUseEdges(umlClass, node, variableDeclarationFragment);
+                addVariableDeclarationEdges(umlClass, node, variableDeclarationFragment);
+            }
+        }
+    }
+
+    private void processClassInstanceCreations() {
+        for (Node node : graph.vertexSet()) {
+            if (node.isContext()) {
+                continue;
+            }
+
+            List<Tree> subTrees = new ArrayList<>();
+            Tree tree = node.getTree();
+            subTrees.add(tree);
+            subTrees.addAll(tree.getDescendants());
+
+            List<Tree> classInstanceCreations =
+                    subTrees.stream().filter(subTree -> subTree.getType().name.equals(Constants.CLASS_INSTANCE_CREATION)).toList();
+            for (Tree classInstanceCreation : classInstanceCreations) {
+                Tree classType = TreeUtilFunctions.findChildByType(classInstanceCreation, Constants.SIMPLE_TYPE);
+                if (classType == null) {
+                    continue;
+                }
+                Tree className = TreeUtilFunctions.findChildByType(classType, Constants.SIMPLE_NAME);
+                if (className == null) {
+                    continue;
+                }
+
+                Optional<UMLClass> instantiatedClass =
+                        modelDiff.getChildModel().getClassList().stream().filter(c -> c.getName().endsWith(className.getLabel())).findFirst();
+                if (instantiatedClass.isEmpty()) {
+                    continue;
+                }
+
+                LocationInfo createdClassLocation = instantiatedClass.get().getLocationInfo();
+                List<Node> classNodes = findOverlappingNodes(createdClassLocation.getFilePath(),
+                        createdClassLocation.getStartOffset(), createdClassLocation.getEndOffset());
+                for (Node classNode : classNodes) {
+                    if (classNode.equals(node) || !classNode.getTree().getType().name.equals(Constants.TYPE_DECLARATION)) {
+                        continue;
+                    }
+
+                    addEdge(node, classNode, EdgeType.DEF_USE, 1);
+                }
+
             }
         }
     }
@@ -172,7 +232,7 @@ public class GraphProcessor {
         return getUMLClass(node.getPath(), parentTypeName.getLabel());
     }
 
-    private void addVariableDeclarationUseEdges(UMLClass umlClass, Node node, Tree variableDeclaration) {
+    private void addVariableDeclarationEdges(UMLClass umlClass, Node node, Tree variableDeclaration) {
         Tree methodRoot = TreeUtilFunctions.getParentUntilType(variableDeclaration, Constants.METHOD_DECLARATION);
         if (methodRoot == null) { // https://github.com/elastic/elasticsearch/commit/e0a458441cff9a4242cd93f4c02f06d72f2d63c4#diff-9b7bef16de393901cd8c75e73d2fb03afb90a10f1de191b7174275bbd8e71bd8L38
             return;
@@ -185,7 +245,8 @@ public class GraphProcessor {
 
         for (VariableDeclaration operationVariable : umlOperation.getAllVariableDeclarations()) {
             LocationInfo operationVariableLoc = operationVariable.getLocationInfo();
-            if (operationVariableLoc.getStartOffset() == variableDeclaration.getPos() && variableDeclaration.getEndPos() == operationVariableLoc.getEndOffset()) {
+            if (operationVariableLoc.getStartOffset() == variableDeclaration.getPos()
+                    && variableDeclaration.getEndPos() == operationVariableLoc.getEndOffset()) {
                 List<Node> useNodes = findUseNodes(operationVariable.getVariableDeclaration());
                 for (Node useNode : useNodes) {
                     addEdge(node, useNode, EdgeType.DEF_USE, 1);
@@ -221,7 +282,53 @@ public class GraphProcessor {
             }
         }
 
+        if (result.isEmpty()) {
+
+            List<LocationInfo> changedFilesUseStatementsLocation = useStatements.stream()
+                    .map(LocationInfoProvider::getLocationInfo)
+                    .filter(locationInfo -> childContexts.containsKey(locationInfo.getFilePath())).toList();
+            if (changedFilesUseStatementsLocation.isEmpty()) {
+                return result;
+            }
+
+            LocationInfo closestLocationInfo = findClosestLocationInfo(declaration.getLocationInfo(), changedFilesUseStatementsLocation);
+            String path = closestLocationInfo.getFilePath();
+            Tree tree = TreeUtilFunctions.findByLocationInfo(childContexts.get(path).getRoot(), closestLocationInfo);
+            Node node = addHunkNode(path, tree, NodeType.EXTENSION);
+
+            result.add(node);
+        }
+
         return result;
+    }
+
+    private LocationInfo findClosestLocationInfo(LocationInfo subject, List<LocationInfo> candidates) {
+        int pos = subject.getStartOffset();
+        int endPos = subject.getEndOffset();
+
+        List<LocationInfo> sameFileCandidates = candidates.stream()
+                .filter(candidate -> subject.getFilePath().equals(candidate.getFilePath())).toList();
+        if (!sameFileCandidates.isEmpty()) {
+            LocationInfo closestLocationInfo = null;
+
+            int shortestDistance = Integer.MAX_VALUE;
+            for (LocationInfo candidate : sameFileCandidates) {
+                if (endPos >= candidate.getStartOffset() && candidate.getEndOffset() >= pos) {
+                    return candidate;
+                }
+
+                int distance = endPos < candidate.getStartOffset() ?
+                        candidate.getStartOffset() - endPos : pos - candidate.getEndOffset();
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    closestLocationInfo = candidate;
+                }
+            }
+
+            return closestLocationInfo;
+        }
+
+        return candidates.get(0);
     }
 
     private List<Node> findInvocationNodes(UMLOperation operation) {
@@ -362,50 +469,6 @@ public class GraphProcessor {
                 if (distance <= distanceThreshold) {
                     addEdge(subject, object, EdgeType.SIMILARITY, 1 / (distance + 1));
                 }
-            }
-        }
-    }
-
-    private void processClassInstanceCreations() {
-        for (Node node : graph.vertexSet()) {
-            if (node.isContext()) {
-                continue;
-            }
-
-            List<Tree> subTrees = new ArrayList<>();
-            Tree tree = node.getTree();
-            subTrees.add(tree);
-            subTrees.addAll(tree.getDescendants());
-
-            List<Tree> classInstanceCreations =
-                    subTrees.stream().filter(subTree -> subTree.getType().name.equals(Constants.CLASS_INSTANCE_CREATION)).toList();
-            for (Tree classInstanceCreation : classInstanceCreations) {
-                Tree classType = TreeUtilFunctions.findChildByType(classInstanceCreation, Constants.SIMPLE_TYPE);
-                if (classType == null) {
-                    continue;
-                }
-                Tree className = TreeUtilFunctions.findChildByType(classType, Constants.SIMPLE_NAME);
-                if (className == null) {
-                    continue;
-                }
-
-                Optional<UMLClass> instantiatedClass =
-                        modelDiff.getChildModel().getClassList().stream().filter(c -> c.getName().endsWith(className.getLabel())).findFirst();
-                if (instantiatedClass.isEmpty()) {
-                    continue;
-                }
-
-                LocationInfo createdClassLocation = instantiatedClass.get().getLocationInfo();
-                List<Node> classNodes = findOverlappingNodes(createdClassLocation.getFilePath(),
-                        createdClassLocation.getStartOffset(), createdClassLocation.getEndOffset());
-                for (Node classNode : classNodes) {
-                    if (classNode.equals(node) || !classNode.getTree().getType().name.equals(Constants.TYPE_DECLARATION)) {
-                        continue;
-                    }
-
-                    addEdge(node, classNode, EdgeType.DEF_USE, 1);
-                }
-
             }
         }
     }

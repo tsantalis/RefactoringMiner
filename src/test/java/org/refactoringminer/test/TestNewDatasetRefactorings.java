f662c7670504995986d51551deb0736545518da6
tsantalis <tsantalis@gmail.com>
2025-02-05 14:40:29 -0500

    Foxed the detection of identical refactorings taking place in different
    source folder. The following commit has identical Extract and Moved Method and Move and Rename Attribute in two different source folders.
    https://github.com/hibernate/hibernate-search/commit/18e9cbf61c5047b600af919c9755a38b96afa9c2

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -44,6 +44,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.SplitMethod.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(2981, 255, 394);
+		test.assertExpectationsWithGitHubAPI(2985, 253, 389);
 	}
 }

9978f5b8a147a4d287074c25e93dc8d7614b6544
tsantalis <tsantalis@gmail.com>
2025-03-09 11:22:37 -0400

    Improved detection for Move and Inline Method in commit
    https://github.com/JetBrains/intellij-community/commit/9690b9b6a1b064e2e57d59144fdbc338b9f8fe54
    platform/ide-core/src/com/intellij/openapi/fileChooser/FileChooserDescriptorFactory.java

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -44,6 +44,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.SplitMethod.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3034, 211, 357);
+		test.assertExpectationsWithGitHubAPI(3041, 209, 356);
 	}
 }

a06ba468f59c571e9f283112b63a806010068443
tsantalis <tsantalis@gmail.com>
2025-05-01 11:48:51 -0400

    Update DiffDriver example for locally cloned repository

--- src/main/java/gui/RunWithLocallyClonedRepository.java
+++ src/main/java/gui/RunWithLocallyClonedRepository.java
@@ -2,27 +2,27 @@ package gui;
 
 import gui.webdiff.DiffDriver;
 import gui.webdiff.WebDiff;
-import org.eclipse.jgit.lib.Repository;
+
 import org.refactoringminer.api.GitService;
 import org.refactoringminer.astDiff.models.ProjectASTDiff;
 import org.refactoringminer.astDiff.utils.URLHelper;
-import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
 import org.refactoringminer.util.GitServiceImpl;
-
-import java.nio.file.Path;
 
 public class RunWithLocallyClonedRepository {
     public static void main(String[] args) throws Exception {
-        String url = "https://github.com/JetBrains/intellij-community/commit/7ed3f273ab0caf0337c22f0b721d51829bb0c877";
+        String url = "https://github.com/danilofes/refactoring-toy-example/commit/36287f7c3b09eff78395267a3ac0d7da067863fd";
         String repo = URLHelper.getRepo(url);
         String commit = URLHelper.getCommit(url);
 
         GitService gitService = new GitServiceImpl();
         String projectName = repo.substring(repo.lastIndexOf("/") + 1, repo.length() - 4);
-        String pathToClonedRepository = "tmp/" + projectName;
-        Repository repository = gitService.cloneIfNotExists(pathToClonedRepository, repo);
+        String pathToClonedRepository = System.getProperty("user.dir") + "/tmp/" + projectName;
+        gitService.cloneIfNotExists(pathToClonedRepository, repo);
 
-        ProjectASTDiff projectASTDiff = new GitHistoryRefactoringMinerImpl().diffAtCommit(repository, commit);
+        DiffDriver diffDriver = new DiffDriver();
+        diffDriver.setRepo(pathToClonedRepository);
+        diffDriver.setCommit(commit);
+        ProjectASTDiff projectASTDiff = diffDriver.getProjectASTDiff();
         new WebDiff(projectASTDiff).run();
     }
 }

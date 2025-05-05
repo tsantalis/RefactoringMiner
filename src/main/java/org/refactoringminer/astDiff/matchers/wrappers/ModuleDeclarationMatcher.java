3a2eade6296e45ae33e67870d150f68a2800db75
Pouryafard75 <pouryafard75@gmail.com>
2025-05-04 21:24:34 -0400

    Fix module names

--- src/main/java/org/refactoringminer/astDiff/matchers/wrappers/ModuleDeclarationMatcher.java
+++ src/main/java/org/refactoringminer/astDiff/matchers/wrappers/ModuleDeclarationMatcher.java
@@ -14,9 +14,25 @@ public class ModuleDeclarationMatcher implements TreeMatcher {
     public void match(Tree srcTree, Tree dstTree, ExtendedMultiMappingStore mappingStore) {
         Tree srcModuleDeclaration = findModuleDeclaration(srcTree);
         Tree dstModuleDeclaration = findModuleDeclaration(dstTree);
-        if (srcModuleDeclaration != null && dstModuleDeclaration != null)
+        if (srcModuleDeclaration != null && dstModuleDeclaration != null) {
             mappingStore.addMappingRecursively(srcModuleDeclaration,dstModuleDeclaration);
+            matchModuleNames(mappingStore, srcModuleDeclaration, dstModuleDeclaration);
+        }
     }
+
+    private static void matchModuleNames(ExtendedMultiMappingStore mappingStore, Tree srcModuleDeclaration, Tree dstModuleDeclaration) {
+        // The name of the module declaration always the first child
+        Tree srcName = srcModuleDeclaration.getChild(0);
+        Tree dstName = dstModuleDeclaration.getChild(0);
+        if (srcName != null && dstName != null) {
+            if (srcName.isIsomorphicTo(dstName)) {
+                mappingStore.addMappingRecursively(srcName, dstName);
+            } else {
+                mappingStore.addMapping(srcName, dstName);
+            }
+        }
+    }
+
     private Tree findModuleDeclaration(Tree inputTree) {
         String searchingType = Constants.MODULE_DECLARATION;
         if (!inputTree.getChildren().isEmpty()) {

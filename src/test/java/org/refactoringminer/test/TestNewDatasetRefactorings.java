e8e9c3a26fa4e4155572f65da2949a664e1fda3a
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-05-27 09:17:35 -0400

    Enable Rename Variable detection in commit
    https://github.com/apache/commons-io/commit/57ab49f51f16dadf6ad246dbbcaad18fa3dd7a0e

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -45,6 +45,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplaceAnonymousWithClass.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3081, 182, 340);
+		test.assertExpectationsWithGitHubAPI(3082, 182, 339);
 	}
 }

9c180c7a0415e4ea5370238a2ae1ff4aafc3ca6f
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-05-31 19:36:13 -0400

    Enable Extract Method detection in commit
    https://github.com/junit-team/junit5/commit/3184a6e350590aa23142cb31d919bd68c257cad4

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -45,6 +45,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplaceAnonymousWithClass.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3097, 177, 332);
+		test.assertExpectationsWithGitHubAPI(3099, 176, 331);
 	}
 }

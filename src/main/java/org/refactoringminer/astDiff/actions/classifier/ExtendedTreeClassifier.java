4cafedf3bc3688c42c3f7484af5647aaef440485
Pouryafard75 <pouryafard75@gmail.com>
2025-06-02 21:46:17 -0400

    Update ASTDiff action classifier API
    Migration from Map<Tree, Action> to Map<Tree, List<Action> for inter-filers.

--- src/main/java/org/refactoringminer/astDiff/actions/classifier/ExtendedTreeClassifier.java
+++ src/main/java/org/refactoringminer/astDiff/actions/classifier/ExtendedTreeClassifier.java
@@ -4,6 +4,7 @@ import com.github.gumtreediff.actions.TreeClassifier;
 import com.github.gumtreediff.actions.model.Action;
 import com.github.gumtreediff.tree.Tree;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,8 +13,8 @@ import java.util.Map;
  * @see Tree
  */
 public interface ExtendedTreeClassifier extends TreeClassifier {
-	Map<Tree, Action> getMultiMapSrc();
-	Map<Tree, Action> getMultiMapDst();
-	Map<Tree, Action> getDstMoveInTreeMap();
-	Map<Tree, Action> getSrcMoveOutTreeMap();
+	Map<Tree, List<Action>> getMultiMapSrc();
+    Map<Tree, List<Action>> getMultiMapDst();
+    Map<Tree, List<Action>> getDstMoveInTreeMap();
+    Map<Tree, List<Action>> getSrcMoveOutTreeMap();
 }

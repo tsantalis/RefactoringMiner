74fb55af74275e276eb5cbd9213c88bac26afa3e
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-06-12 16:14:48 -0400

    Fix failing test

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -45,6 +45,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplaceAnonymousWithClass.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3130, 172, 307);
+		test.assertExpectationsWithGitHubAPI(3131, 172, 306);
 	}
 }

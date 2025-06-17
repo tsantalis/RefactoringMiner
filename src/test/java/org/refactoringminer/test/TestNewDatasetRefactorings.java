30943e92a132834d14270a7117c5d0bae8d1848c
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-06-17 13:12:18 -0400

    Temporary fix for #939

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -45,6 +45,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplaceAnonymousWithClass.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3146, 169, 292);
+		test.assertExpectationsWithGitHubAPI(3146, 169, 291);
 	}
 }

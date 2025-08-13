5852f3214172adc97d09717e6ad24d8381aaa937
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-08-13 13:50:25 -0400

    Improve Rename Variable detection in commit
    https://github.com/hibernate/hibernate-search/commit/5b7ff9004b3b9f7ec8b3463f33941d2fe46cce7c

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3209, 134, 233);
+		test.assertExpectationsWithGitHubAPI(3211, 134, 231);
 	}
 }

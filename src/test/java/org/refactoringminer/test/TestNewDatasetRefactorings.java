6694803c9eda3f766d402adac3570b34ea64d4ff
tsantalis <tsantalis@gmail.com>
2025-08-14 14:51:57 -0400

    Fixed Rename Class detection in commit
    https://github.com/hibernate/hibernate-search/commit/b2a9f0a25e63ade9bbab5b4c669d07bb0cf47442

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3214, 131, 230);
+		test.assertExpectationsWithGitHubAPI(3216, 130, 228);
 	}
 }

64db64efa0f1a00b53539f0d47c114cdc5caa23e
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-08-15 12:42:58 -0400

    Enable Rename Attribute detection in commit
    https://github.com/hibernate/hibernate-search/commit/d114775f2de0cb81684152bfe90898597f091947

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3216, 130, 228);
+		test.assertExpectationsWithGitHubAPI(3218, 130, 226);
 	}
 }

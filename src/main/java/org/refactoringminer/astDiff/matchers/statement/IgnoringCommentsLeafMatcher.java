38a57f186fd701565fb156fc706e0784c05c9b67
Pouryafard75 <pouryafard75@gmail.com>
2025-08-20 13:04:32 -0400

    ASTDiff: Fixes the annotation processor

---
+++ src/main/java/org/refactoringminer/astDiff/matchers/statement/IgnoringCommentsLeafMatcher.java
@@ -0,0 +1,29 @@
+package org.refactoringminer.astDiff.matchers.statement;
+
+import com.github.gumtreediff.matchers.Mapping;
+import com.github.gumtreediff.tree.Tree;
+import org.refactoringminer.astDiff.models.ExtendedMultiMappingStore;
+import org.refactoringminer.astDiff.utils.Constants;
+import org.refactoringminer.astDiff.utils.TreeUtilFunctions;
+
+public class IgnoringCommentsLeafMatcher extends LeafMatcher {
+    @Override
+    public void match(Tree src, Tree dst, ExtendedMultiMappingStore mappingStore) {
+        ExtendedMultiMappingStore newMappings = new ExtendedMultiMappingStore(src, dst);
+        super.match(src, dst, newMappings);
+        // Remove comment mappings from the mapping store
+        for (Mapping newMapping : newMappings) {
+            if (isPartOfComments(newMapping.first) || isPartOfComments(newMapping.second)) {
+                newMappings.removeMapping(newMapping.first, newMapping.second);
+            }
+        }
+        for (Mapping newMapping : newMappings) {
+            mappingStore.addMapping(newMapping.first, newMapping.second);
+        }
+    }
+    public static boolean isPartOfComments(Tree tree) {
+        return TreeUtilFunctions.getParentUntilType(tree, Constants.LINE_COMMENT) != null ||
+               TreeUtilFunctions.getParentUntilType(tree, Constants.JAVA_DOC) != null ||
+               TreeUtilFunctions.getParentUntilType(tree, Constants.BLOCK_COMMENT) != null;
+    }
+}

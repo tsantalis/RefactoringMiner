c618507eca0fc7bd584199ca0e32322feed659fe
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-08-24 18:44:01 -0400

    Fix for issue #985

--- src/main/java/gr/uom/java/xmi/annotation/source/MethodSourceAnnotation.java
+++ src/main/java/gr/uom/java/xmi/annotation/source/MethodSourceAnnotation.java
@@ -1,7 +1,9 @@
 package gr.uom.java.xmi.annotation.source;
 
 import gr.uom.java.xmi.*;
+import gr.uom.java.xmi.decomposition.AbstractCall;
 import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
+import gr.uom.java.xmi.decomposition.AbstractExpression;
 import gr.uom.java.xmi.decomposition.StatementObject;
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
 import gr.uom.java.xmi.annotation.MarkerAnnotation;
@@ -11,12 +13,14 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class MethodSourceAnnotation extends SourceAnnotation implements SingleMemberAnnotation, MarkerAnnotation {
-    private static final String ANNOTATION_TYPENAME = "MethodSource";
+    public static final String ANNOTATION_TYPENAME = "MethodSource";
     private final UMLOperation annotatedOperation;
+    private List<List<String>> testParameters;
 
     public MethodSourceAnnotation(UMLAnnotation annotation, UMLOperation operation, UMLModel model) {
         super(annotation, ANNOTATION_TYPENAME);
-        annotatedOperation = operation;
+        this.annotatedOperation = operation;
+        this.testParameters = new ArrayList<>();
         List<String> values = getValue();
         assert values.size() == 1;
         String methodSourceName = values.get(0);
@@ -38,36 +42,58 @@ public class MethodSourceAnnotation extends SourceAnnotation implements SingleMe
                 }
             }
         }
-        UMLOperation sourceMethod = sameNameMethods.get(0);
-        Optional<VariableDeclaration> returnedVarCandidates = sourceMethod.getBody().getAllVariableDeclarations().stream().filter(v -> sourceMethod.getReturnParameter().getType().equals(v.getType())).findAny();
-        String strLiterals;
-        if (returnedVarCandidates.isPresent()) {
-            Set<AbstractCodeFragment> stmtsUsingVar = returnedVarCandidates.get().getStatementsInScopeUsingVariable();
-            strLiterals = stmtsUsingVar.stream()
-                    .flatMap(stmt -> stmt.getStringLiterals().stream())
-                    .map(str -> str.getString())
-                    .collect(Collectors.joining(System.getProperty("line.separator")));
-        } else {
-            Optional<StatementObject> stmtCandidate = sourceMethod.getBody().getCompositeStatement().getStatements().stream()
-                    .filter(s -> s instanceof StatementObject)
-                    .map(s -> (StatementObject) s)
-                    .filter(s -> s.isLastStatement())
-                    .findAny();
-            if (stmtCandidate.isPresent()) {
-                strLiterals = stmtCandidate.get().getStringLiterals().stream()
-                        .map(str -> str.getString())
-                        .collect(Collectors.joining(System.getProperty("line.separator")));
-            }
+        if(sameNameMethods.size() > 0) {
+	        UMLOperation sourceMethod = sameNameMethods.get(0);
+	        Optional<VariableDeclaration> returnedVarCandidates = sourceMethod.getBody().getAllVariableDeclarations().stream().filter(v -> sourceMethod.getReturnParameter().getType().equals(v.getType())).findAny();
+	        String strLiterals;
+	        if (returnedVarCandidates.isPresent()) {
+	            Set<AbstractCodeFragment> stmtsUsingVar = returnedVarCandidates.get().getStatementsInScopeUsingVariable();
+	            strLiterals = stmtsUsingVar.stream()
+	                    .flatMap(stmt -> stmt.getStringLiterals().stream())
+	                    .map(str -> str.getString())
+	                    .collect(Collectors.joining(System.getProperty("line.separator")));
+	        } else {
+	            Optional<StatementObject> stmtCandidate = sourceMethod.getBody().getCompositeStatement().getStatements().stream()
+	                    .filter(s -> s instanceof StatementObject)
+	                    .map(s -> (StatementObject) s)
+	                    .filter(s -> s.isLastStatement())
+	                    .findAny();
+	            if (stmtCandidate.isPresent()) {
+	                strLiterals = stmtCandidate.get().getStringLiterals().stream()
+	                        .map(str -> str.getString())
+	                        .collect(Collectors.joining(System.getProperty("line.separator")));
+	                AbstractCall call = stmtCandidate.get().invocationCoveringEntireFragment();
+	                if(call != null && call.getName().equals("of")) {
+	                    for(AbstractCall nestedCall : stmtCandidate.get().getMethodInvocations()) {
+	                        if(nestedCall.getExpression() != null && !nestedCall.getExpression().equals("Stream") && nestedCall.getName().equals("of")) {
+	                            testParameters.add(nestedCall.arguments());
+	                        }
+	                    }
+	                }
+	            }
+	        }
         }
     }
 
     @Override
     public List<String> getValue() {
-        return Collections.singletonList(annotation.isMarkerAnnotation() ? annotatedOperation.getName() : annotation.getValue().getTypeLiterals().get(0).getString());
+        if(annotation.isMarkerAnnotation()) {
+            return Collections.singletonList(annotatedOperation.getName());
+        }
+        else {
+            AbstractExpression expr = annotation.getValue();
+            if(expr.getTypeLiterals().size() > 0) {
+                return Collections.singletonList(expr.getTypeLiterals().get(0).getString());
+            }
+            else if (expr.getStringLiterals().size() > 0) {
+                return Collections.singletonList(expr.getStringLiterals().get(0).getString().replace("\"", ""));
+            }
+        }
+        return Collections.emptyList();
     }
 
     @Override
     public List<List<String>> getTestParameters() {
-        return null;
+        return testParameters;
     }
 }

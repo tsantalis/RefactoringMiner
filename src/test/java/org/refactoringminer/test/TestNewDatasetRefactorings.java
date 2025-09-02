bbf38f55f9950c605cc078549973d157b76c8062
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-09-02 12:51:27 -0400

    Improve detection of Extract/Inline variable refactorings to unmatched
    statements
    https://github.com/JetBrains/intellij-community/commit/9690b9b6a1b064e2e57d59144fdbc338b9f8fe54
    
    Consumer<Dialog> dialogConsumer = owner -> myFileDialog = new FileDialog(owner, title, FileDialog.SAVE);
    Consumer<Frame> frameConsumer = owner -> myFileDialog = new FileDialog(owner, title, FileDialog.SAVE);
    
    Pending False positives to be validated

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3238, 120, 207);
+		test.assertExpectationsWithGitHubAPI(3241, 122, 206);
 	}
 }

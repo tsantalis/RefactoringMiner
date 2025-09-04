c406fd85cf209ab7fc84327355e6969a4be74370
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-09-04 07:30:33 -0400

    Initial fix for issue #996

--- src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
+++ src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
@@ -7129,7 +7129,7 @@ public class UMLOperationBodyMapper implements Comparable<UMLOperationBodyMapper
 							}
 							else {
 								boolean isTestMethod = (container1.hasTestAnnotation() || container2.hasTestAnnotation() || container1.getName().startsWith("test") || container2.getName().startsWith("test"))
-										&& !container2.hasParameterizedTestAnnotation();
+										&& !container2.hasParameterizedTestAnnotation() && leaves2.size() < 2*leaves1.size();
 								boolean mappingWithNestedLambdaMapper = mappingSet.first().getLambdaMappers().size() > 0/* && mappingSet.first().nestedLambdaMappers().size() > 0*/;
 								if(!isTestMethod && !allIdenticalStatementsHaveSameIndex && !mappingWithNestedLambdaMapper)
 									checkForOtherPossibleMatchesForFragment2(leaves1, leaves2, leaf1, mappingSet, parameterToArgumentMap, equalNumberOfAssertions);

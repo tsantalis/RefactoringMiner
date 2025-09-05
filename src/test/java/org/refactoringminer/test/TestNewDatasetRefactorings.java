bd8a559521064232b9358139fa7ec49291e21730
tsantalis <tsantalis@gmail.com>
2025-09-05 13:01:00 -0400

    Correction in benchmark 2
    https://github.com/apache/commons-lang/commit/3935fa2f193a0cc652300677beeb61b535eedf4c

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3250, 111, 203);
+		test.assertExpectationsWithGitHubAPI(3251, 110, 201);
 	}
 }

7e1061ec1a650a7249d9980f2d0eff0f95a536c5
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-09-11 15:02:57 -0400

    Fix failing test
    defects4J/JxPath/16 should be moved to perfect diffs

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3265, 108, 192);
+		test.assertExpectationsWithGitHubAPI(3266, 107, 192);
 	}
 }

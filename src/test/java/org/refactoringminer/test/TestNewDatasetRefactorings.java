7683d4fa34986b33c5b9a0d0a6223a3aeac50ef2
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-09-15 05:49:12 -0400

    Enable Move and Rename Method detection in commit
    https://github.com/hibernate/hibernate-search/commit/c4cb8b6876c27cc1364250ce70e8155bdd2c5748

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3270, 103, 189);
+		test.assertExpectationsWithGitHubAPI(3271, 103, 188);
 	}
 }

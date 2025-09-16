8f74cb0387083b146127c588f5cfa41c026b075d
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-09-16 15:36:05 -0400

    Correction in benchmark 2
    https://github.com/eclipse-jgit/jgit/commit/f94be665f12d85304db056c6cb8934870de3e45a

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -48,6 +48,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplacePipelineWithLoop.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3287, 91, 183);
+		test.assertExpectationsWithGitHubAPI(3288, 90, 183);
 	}
 }

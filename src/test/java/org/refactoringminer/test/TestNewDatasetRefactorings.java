59f3ec3a763143d8dfabeb29c94f8921eaaeaf51
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-10-28 07:26:59 -0400

    Improve inference-based refactoring detection in commit
    https://github.com/spring-projects/spring-boot/commit/793aca60d2d6ad4ff1618bc19672038cd4755120
    Eliminates FP
    Rename Method public getLayer(entry ZipEntry) : String renamed to public getApplicationLayerName() : String in class org.springframework.boot.jarmode.tools.Layers

--- src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestNewDatasetRefactorings.java
@@ -50,6 +50,6 @@ public class TestNewDatasetRefactorings {
 				.or(Refactorings.ReplaceAttributeWithVariable.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.feedTSERefactoringInstances(test);
-		test.assertExpectationsWithGitHubAPI(3393, 63, 132);
+		test.assertExpectationsWithGitHubAPI(3395, 60, 132);
 	}
 }

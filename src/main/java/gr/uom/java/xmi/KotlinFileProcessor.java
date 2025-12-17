bab0f2771008ebf8d023e25773e4a86a2fdbc900
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-12-17 18:12:03 -0500

    process package and imports in Kotlin

---
+++ src/main/java/gr/uom/java/xmi/KotlinFileProcessor.java
@@ -0,0 +1,63 @@
+package gr.uom.java.xmi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jetbrains.kotlin.com.intellij.psi.PsiFile;
+import org.jetbrains.kotlin.com.intellij.psi.impl.PsiFileFactoryImpl;
+import org.jetbrains.kotlin.idea.KotlinLanguage;
+import org.jetbrains.kotlin.name.FqName;
+import org.jetbrains.kotlin.psi.KtElement;
+import org.jetbrains.kotlin.psi.KtFile;
+import org.jetbrains.kotlin.psi.KtImportDirective;
+import org.jetbrains.kotlin.psi.KtImportList;
+import org.jetbrains.kotlin.psi.KtPackageDirective;
+
+import gr.uom.java.xmi.LocationInfo.CodeElementType;
+
+public class KotlinFileProcessor {
+	private UMLModel umlModel;
+
+	public KotlinFileProcessor(UMLModel umlModel) {
+		this.umlModel = umlModel;
+	}
+
+	public void processKotlinFile(String filePath, String fileContent, boolean astDiff, PsiFileFactoryImpl factory) {
+		PsiFile psiFile = factory.createFileFromText(filePath, KotlinLanguage.INSTANCE, fileContent);
+		KtFile ktFile = (KtFile)psiFile;
+		
+		String packageName = "";
+		String sourceFolder = "";
+		KtPackageDirective packageDirective = ktFile.getPackageDirective();
+		if(packageDirective != null) {
+			packageName = packageDirective.getQualifiedName();
+			int index = filePath.indexOf(packageName.replace('.', '/'));
+			if(index != -1) {
+				sourceFolder = filePath.substring(0, index);
+			}
+		}
+		
+		List<UMLImport> importedTypes = new ArrayList<UMLImport>();
+		KtImportList importList = ktFile.getImportList();
+		if (importList != null) {
+			for (KtImportDirective importDeclaration : importList.getImports()) {
+				FqName fqName = importDeclaration.getImportedFqName();
+				String importName = fqName == null ? null : fqName.asString();
+				if (importName != null) {
+					LocationInfo locationInfo = generateLocationInfo(ktFile, sourceFolder, filePath, importDeclaration, CodeElementType.IMPORT_DECLARATION);
+					// kotlin does not have static keyword for method imports, you can import directly a method
+					UMLImport imported = new UMLImport(importName, importDeclaration.isAllUnder(), false, locationInfo);
+					importedTypes.add(imported);
+				}
+			}
+		}
+	}
+
+	private LocationInfo generateLocationInfo(KtFile ktFile,
+			String sourceFolder,
+			String sourceFilePath,
+			KtElement node,
+			CodeElementType codeElementType) {
+		return new LocationInfo(ktFile, sourceFolder, sourceFilePath, node, codeElementType);
+	}
+}

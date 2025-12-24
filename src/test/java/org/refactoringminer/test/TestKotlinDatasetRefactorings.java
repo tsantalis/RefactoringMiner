d00199c9609a068ead31440a10080107832311ee
Nikolaos Tsantalis <tsantalis@gmail.com>
2025-12-24 14:49:01 -0500

    Add 1 commit in Kotlin commit tests

--- src/test/java/org/refactoringminer/test/TestKotlinDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestKotlinDatasetRefactorings.java
@@ -54,6 +54,6 @@ public class TestKotlinDatasetRefactorings {
 					.or(Refactorings.SplitParameter.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.prepareKotlinRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(1, 0, 0);
+		test.assertExpectationsWithGitHubAPI(3, 0, 0);
 	}
 }

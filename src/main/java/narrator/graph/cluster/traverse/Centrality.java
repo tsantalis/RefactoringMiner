82f43361b08432704b6fe266071fbf5b40d93e5a
maorethians <apaladian@gmail.com>
2026-01-10 20:47:13 -0500

    fix: support extension based usage patterns

--- src/main/java/narrator/graph/cluster/traverse/Centrality.java
+++ src/main/java/narrator/graph/cluster/traverse/Centrality.java
@@ -1,16 +1,19 @@
 package narrator.graph.cluster.traverse;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import narrator.graph.Edge;
 import narrator.graph.EdgeType;
 import narrator.graph.Node;
 import org.jgrapht.Graph;
 
-import java.util.*;
-
 public class Centrality {
+
     //    Nodes with the most connections (highest degree) are considered central.
     public static List<Node> degree(Graph<Node, Edge> graph) {
-        List<Node> hunkNodes = graph.vertexSet().stream().filter(node -> !node.isContext()).toList();
+        List<Node> hunkNodes = graph.vertexSet().stream().filter(node -> !node.isContext())
+                .toList();
 
         HashMap<Node, Integer> nodesDegree = new HashMap<>();
         for (Node node : hunkNodes) {
@@ -24,17 +27,22 @@ public class Centrality {
 
     //      Nodes with most used declarations
     public static List<Node> usedDeclarations(Graph<Node, Edge> graph) {
-        List<Node> hunkNodes = graph.vertexSet().stream().filter(node -> !node.isContext()).toList();
+        List<Node> hunkNodes = graph.vertexSet().stream().filter(node -> !node.isContext())
+                .toList();
 
         HashMap<Node, Integer> nodesUsedDeclarations = new HashMap<>();
         for (Node node : hunkNodes) {
             List<Edge> usedDeclarations = graph.incomingEdgesOf(node).stream()
                     .filter(edge -> edge.getType().equals(EdgeType.DEF_USE))
-                    .filter(edge -> graph.getEdgeSource(edge).isBase()).toList();
+                    .filter(edge -> {
+                        Node edgeSource = graph.getEdgeSource(edge);
+                        return edgeSource.isBase() || edgeSource.isExtension();
+                    })
+                    .toList();
             if (usedDeclarations.isEmpty()) {
                 continue;
             }
-            
+
             nodesUsedDeclarations.put(node, usedDeclarations.size());
         }
 

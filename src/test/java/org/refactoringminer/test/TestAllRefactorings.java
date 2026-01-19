dd74bd580e60d49bc908b2b6abb8c3f5fc37d478
Nikolaos Tsantalis <tsantalis@gmail.com>
2026-01-19 07:07:44 -0500

    Enable detection of Rename Method in Kotlin commit
    https://github.com/square/leakcanary/commit/50b3d17592d332df612daf18e04fdcea9651011c
    leakcanary-android-core/src/main/java/leakcanary/internal/activity/screen/LeakScreen.kt
    private fun View.displayLeakTrace(projection: LeakDetails)
    private fun View.onLeakTraceSelected(analysis: HeapAnalysisSuccess, heapAnalysisId: Long, leakTraceIndex: Int)

--- src/test/java/org/refactoringminer/test/TestAllRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestAllRefactorings.java
@@ -17,6 +17,6 @@ public class TestAllRefactorings {
 		GitHistoryRefactoringMinerImpl detector = new GitHistoryRefactoringMinerImpl();
 		TestBuilder test = new TestBuilder(detector, REPOS, Refactorings.All.getValue());
 		RefactoringPopulator.feedRefactoringsInstances(Refactorings.All.getValue(), Systems.FSE.getValue(), test);
-		test.assertExpectationsWithGitHubAPI(12614, 13, 215);
+		test.assertExpectationsWithGitHubAPI(12638, 13, 212);
 	}
 }

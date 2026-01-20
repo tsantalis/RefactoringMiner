dd203dd8bdc3b8b664a72e44deec02d781889c67
Nikolaos Tsantalis <tsantalis@gmail.com>
2026-01-20 16:10:46 -0500

    Add 1 validated commit in Kotlin commit tests

--- src/test/java/org/refactoringminer/test/TestKotlinDatasetRefactorings.java
+++ src/test/java/org/refactoringminer/test/TestKotlinDatasetRefactorings.java
@@ -76,6 +76,6 @@ public class TestKotlinDatasetRefactorings {
 					.or(Refactorings.ChangeClassAccessModifier.getValue());
 		TestBuilder test = new TestBuilder(detector, REPOS, types);
 		RefactoringPopulator.prepareKotlinRefactorings(test, types);
-		test.assertExpectationsWithGitHubAPI(1328, 3, 0);
+		test.assertExpectationsWithGitHubAPI(1338, 3, 0);
 	}
 }

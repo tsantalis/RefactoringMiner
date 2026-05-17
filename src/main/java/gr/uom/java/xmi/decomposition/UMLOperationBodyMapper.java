261d01666ccd7ed03e593612fa5768126405970c
Nikolaos Tsantalis <tsantalis@gmail.com>
2026-05-16 21:30:03 -0400

    Eliminate false Replace Loop with Pipeline instance
    https://github.com/colbymchenry/codegraph/commit/453c39d774d48c2067d831442ad2d6f7ef63915a
    src/context/index.ts

--- src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
+++ src/main/java/gr/uom/java/xmi/decomposition/UMLOperationBodyMapper.java
@@ -919,7 +919,9 @@ public class UMLOperationBodyMapper implements Comparable<UMLOperationBodyMapper
 			else if(streamAPIStatements1.size() > 0 && streamAPIStatements2.size() == 0) {
 				processStreamAPIStatements(leaves1, leaves2, streamAPIStatements1, innerNodes2);
 			}
-			else if(streamAPIStatements1.size() > 1 || streamAPIStatements2.size() > 1) {
+			else if((streamAPIStatements1.size() > 1 || streamAPIStatements2.size() > 1) &&
+					!streamAPIStatements1.stream().map(s -> s.getString()).collect(Collectors.toList()).equals(
+					streamAPIStatements2.stream().map(s -> s.getString()).collect(Collectors.toList()))) {
 				processStreamAPIStatements(leaves1, leaves2, innerNodes1, streamAPIStatements2);
 			}
 			else if(streamAPIStatements1.size() == 1 && streamAPIStatements2.size() == 1 && nestedLambdas1.size() != nestedLambdas2.size()) {

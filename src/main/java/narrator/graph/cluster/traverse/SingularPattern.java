23db2eca7627e72461784f94f50b743a01fd1738
maorethians <apaladian@gmail.com>
2026-05-19 16:08:41 -0400

    feat: revert singular prompt

--- src/main/java/narrator/graph/cluster/traverse/SingularPattern.java
+++ src/main/java/narrator/graph/cluster/traverse/SingularPattern.java
@@ -2,7 +2,6 @@ package narrator.graph.cluster.traverse;
 
 import narrator.graph.Node;
 import narrator.graph.NodeType;
-import narrator.graph.Edge;
 import narrator.graph.cluster.Cluster;
 
 public class SingularPattern extends TraversalPattern implements Leaf {
@@ -26,28 +25,6 @@ public class SingularPattern extends TraversalPattern implements Leaf {
 
     @Override
     public String base(Cluster cluster) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(node.base(cluster));
-
-        org.jgrapht.Graph<Node, Edge> graph = cluster.getGraph();
-        java.util.Set<Edge> edges = new java.util.HashSet<>(graph.edgesOf(node));
-
-        if (!edges.isEmpty()) {
-            sb.append("\n\n----------------------------------------------------------------------\n");
-            sb.append("RELATED CONTEXT:\n");
-            sb.append("The following nodes are connected to this change and may be required for a full explanation.\n");
-            sb.append("Request them by ID to see their content:\n");
-            for (Edge edge : edges) {
-                Node connectedNode = graph.getEdgeSource(edge).equals(node) 
-                    ? graph.getEdgeTarget(edge) 
-                    : graph.getEdgeSource(edge);
-                sb.append("\n- ").append(connectedNode.getPromptId())
-                  .append(" (").append(connectedNode.getNodeType())
-                  .append(") [Relation: ").append(edge.getType()).append("]");
-            }
-            sb.append("\n----------------------------------------------------------------------");
-        }
-
-        return sb.toString();
+        return node.base(cluster);
     }
 }

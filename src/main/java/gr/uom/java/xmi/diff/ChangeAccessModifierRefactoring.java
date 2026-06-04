0f1edb288a95ed8e560757b9fc39975ae250eab3
Nikolaos Tsantalis <tsantalis@gmail.com>
2026-06-04 12:50:43 -0400

    23 toString() implementations merged into AbstractRefactoring

--- src/main/java/gr/uom/java/xmi/diff/ChangeAccessModifierRefactoring.java
+++ src/main/java/gr/uom/java/xmi/diff/ChangeAccessModifierRefactoring.java
@@ -1,41 +1,12 @@
 package gr.uom.java.xmi.diff;
 
-import gr.uom.java.xmi.AnnotationProvider;
-import gr.uom.java.xmi.VariableDeclarationContainer;
+import java.util.Optional;
+
 import gr.uom.java.xmi.Visibility;
-import gr.uom.java.xmi.decomposition.VariableDeclaration;
 
 public abstract class ChangeAccessModifierRefactoring extends AbstractRefactoring {
 	public abstract Visibility getOriginalAccessModifier();
 	public abstract Visibility getChangedAccessModifier();
-	public String getTemplateParameter() {return getChangedAccessModifier().toString();}
-
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()).append("\t");
-		sb.append(getOriginalAccessModifier());
-		sb.append(" to ");
-		sb.append(getChangedAccessModifier());
-		AnnotationProvider provider = getProviderAfter();
-		String codeElementType = codeElementType(provider);
-		sb.append(" in ").append(codeElementType).append(" ");
-		sb.append(codeElementDescription(provider));
-		String className = null;
-		if(provider instanceof VariableDeclaration) {
-			MethodLevelRefactoring methodLevelRef = (MethodLevelRefactoring)this;
-			VariableDeclarationContainer container = methodLevelRef.getOperationAfter();
-			String elementType = container.getElementType();
-			sb.append(" in " + elementType + " ");
-			sb.append(container.toQualifiedString());
-			className = container.getClassName();
-		}
-		else {
-			className = provider.getClassName();
-		}
-		if(!codeElementType.equals("class")) {
-			sb.append(" from class ");
-			sb.append(className);
-		}
-		return sb.toString();
-	}
+	public Optional<String> getTemplateParameterBefore() {return Optional.of(getOriginalAccessModifier().toString());}
+	public String getTemplateParameterAfter() {return getChangedAccessModifier().toString();}
 }
--- src/main/java/gr/uom/java/xmi/diff/ModifierRefactoring.java
+++ src/main/java/gr/uom/java/xmi/diff/ModifierRefactoring.java
@@ -1,9 +1,12 @@
 package gr.uom.java.xmi.diff;
 
+import java.util.Optional;
+
 import gr.uom.java.xmi.UMLModifier;
 
 public abstract class ModifierRefactoring extends AbstractRefactoring {
 	public abstract UMLModifier getModifier();
 	public abstract String getModifierAsString();
-	public String getTemplateParameter() {return getModifierAsString();}
+	public Optional<String> getTemplateParameterBefore() {return Optional.empty();}
+	public String getTemplateParameterAfter() {return getModifierAsString();}
 }

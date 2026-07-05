ffacadb739ef75cfe392e515b2ada0d2d03613f5
Nikolaos Tsantalis <tsantalis@gmail.com>
2026-07-05 10:50:06 -0400

    Support matching macros with changes in their name, but identical values

--- src/main/java/gr/uom/java/xmi/diff/UMLPreprocessorStatementListDiff.java
+++ src/main/java/gr/uom/java/xmi/diff/UMLPreprocessorStatementListDiff.java
@@ -45,6 +45,52 @@ public class UMLPreprocessorStatementListDiff {
 				}
 			}
 		}
+		else if(newStatements.size() < oldStatements.size()) {
+			Iterator<UMLPreprocessorStatement> it = newStatementsCopy.iterator();
+			while(it.hasNext()) {
+				UMLPreprocessorStatement statement = it.next();
+				int oldIndexOf = oldStatementsCopy.indexOf(statement);
+				int newIndexOf = newStatementsCopy.indexOf(statement);
+				if(oldIndexOf != -1 && newIndexOf != -1) {
+					UMLPreprocessorStatement oldStatement = oldStatementsCopy.get(oldIndexOf);
+					UMLPreprocessorStatement newStatement = newStatementsCopy.get(newIndexOf);
+					Pair<UMLPreprocessorStatement, UMLPreprocessorStatement> pair = Pair.of(oldStatement, newStatement);
+					if(oldStatement.getValue().isPresent() && newStatement.getValue().isPresent()) {
+						if(oldStatement.getValue().get().equals(newStatement.getValue().get())) {
+							commonStatements.add(pair);
+						}
+						else {
+							changedStatements.add(pair);
+						}
+					}
+					else {
+						commonStatements.add(pair);
+					}
+					it.remove();
+					oldStatementsCopy.remove(oldIndexOf);
+				}
+			}
+		}
+		//check if some of the remaining statements have equal values
+		List<UMLPreprocessorStatement> oldToBeRemoved = new ArrayList<UMLPreprocessorStatement>();
+		List<UMLPreprocessorStatement> newToBeRemoved = new ArrayList<UMLPreprocessorStatement>();
+		for(UMLPreprocessorStatement oldStatement : oldStatementsCopy) {
+			if(oldStatement.getValue().isPresent()) {
+				for(UMLPreprocessorStatement newStatement : newStatementsCopy) {
+					if(newStatement.getValue().isPresent()) {
+						if(oldStatement.getValue().get().equals(newStatement.getValue().get())) {
+							Pair<UMLPreprocessorStatement, UMLPreprocessorStatement> pair = Pair.of(oldStatement, newStatement);
+							changedStatements.add(pair);
+							oldToBeRemoved.add(oldStatement);
+							newToBeRemoved.add(newStatement);
+							break;
+						}
+					}
+				}
+			}
+		}
+		oldStatementsCopy.removeAll(oldToBeRemoved);
+		newStatementsCopy.removeAll(newToBeRemoved);
 		this.removedStatements = oldStatementsCopy;
 		this.addedStatements = newStatementsCopy;
 	}

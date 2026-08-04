f12f7b767300176dbac639bf53f07a1a632660b3
maorethians <apaladian@gmail.com>
2026-08-04 16:16:03 -0400

    feat: add semantic context for ellipsis and slice

--- src/main/java/org/refactoringminer/astDiff/utils/Constants.java
+++ src/main/java/org/refactoringminer/astDiff/utils/Constants.java
@@ -606,7 +606,7 @@ public class Constants {
     public final String FIELD_EXPRESSION = "field_expression";
     public final String SUBSCRIPT_EXPRESSION = "subscript_expression";
     public final String ENUMERATOR = "enumerator";
-    public final String ELLIPSIS = "...";
+    public final String ELLIPSIS = "ellipsis";
     public final String STORAGE_CLASS_SPECIFIER = "storage_class_specifier";
     public final String DEFAULT_METHOD_CLAUSE = "default_method_clause";
     public final String DELETE_METHOD_CLAUSE = "delete_method_clause";
